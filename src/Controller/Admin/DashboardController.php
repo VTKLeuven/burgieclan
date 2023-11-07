0fc47404c2bf3138d04b12dc80efa13e95861c0a
stijn kuppens <stijn_kuppens@hotmail.com>
2023-11-07 20:11:45 +0100

    BURGIECLAN-1 Opzet local symfony + easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\PostCrudController;
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\Tag;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect($adminUrlGenerator->setController(PostCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Burgiclan');
+    }
+
+    public function configureCrud(): Crud
+    {
+        return Crud::new()
+            ->setDateTimeFormat('medium', 'short');
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setName($user->getFullName());
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
+        yield MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text-o', Post::class);
+        yield MenuItem::linkToCrud('Comments', 'far fa-comments', Comment::class);
+        yield MenuItem::linkToCrud('Tags', 'fas fa-tags', Tag::class);
+
+        yield MenuItem::section('Frontend');
+        yield MenuItem::linkToUrl('Home', 'fa fa-window-maximize', '/');
+
+        yield MenuItem::section('Resources');
+        yield MenuItem::linkToUrl('EasyAdmin Docs', 'fas fa-book', 'https://symfony.com/doc/current/bundles/EasyAdminBundle/index.html')->setLinkTarget('_blank');
+
+        yield MenuItem::section('Links');
+        yield MenuItem::linkToUrl('Symfony Demo', 'fab fa-symfony', 'https://github.com/symfony/demo')->setLinkTarget('_blank');
+        yield MenuItem::linkToUrl('Symfony Cast - Easy Admin', 'fab fa-symfony', 'https://symfonycasts.com/screencast/easyadminbundle')->setLinkTarget('_blank');
+    }
+}
