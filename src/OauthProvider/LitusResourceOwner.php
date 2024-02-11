aae854b31b0342e3f48c9c85358db47e48bfdce8
Robbe Serry <robbeserry@gmail.com>
2024-02-11 15:08:23 +0000

    BUR-10 refactor code

--- src/OauthProvider/LitusResourceOwner.php
+++ src/OauthProvider/LitusResourceOwner.php
@@ -34,7 +34,6 @@ class LitusResourceOwner implements ResourceOwnerInterface
      */
     public function getId(): ?string
     {
-//        return $this->getValueByKey($this->response, 'id');
         return $this->getUsername();
     }
 
--- src/Security/LitusAuthenticator.php
+++ src/Security/LitusAuthenticator.php
@@ -2,11 +2,8 @@
 
 namespace App\Security;
 
-use App\Entity\User;
 use App\OauthProvider\LitusResourceOwner;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -23,7 +20,6 @@ class LitusAuthenticator extends OAuth2Authenticator implements AuthenticationEn
 {
     public function __construct(
         private readonly ClientRegistry $clientRegistry,
-        private readonly EntityManagerInterface $em,
         private readonly RouterInterface $router,
         private readonly UserRepository $userRepository
     ) {
@@ -48,29 +44,7 @@ class LitusAuthenticator extends OAuth2Authenticator implements AuthenticationEn
                 /** @var LitusResourceOwner $litusUser */
                 $litusUser = $client->fetchUserFromToken($accessToken);
 
-                $user = $this->userRepository->findOneBy(["username" => $litusUser->getUsername()]);
-                if (null === $user) {
-                    $user = $this->userRepository->findOneBy(["email" => $litusUser->getEmail()]);
-                    if (null === $user) {
-                        $user = new User();
-                        $user->setUsername($litusUser->getUsername());
-                        $user->setEmail($litusUser->getEmail());
-                        $user->setFullName($litusUser->getFullName());
-                        $user->setPassword('');
-                        $user->setRoles([User::ROLE_USER]);
-
-                        $this->em->persist($user);
-                    } else {
-                        // Person trying to create an account has the same email as an existing user -> not allowed.
-                        throw new AuthenticationException();
-                    }
-                }
-
-                $user->setAccessToken($accessToken);
-
-                $this->em->flush();
-
-                return $user;
+                return $this->userRepository->createUserfromLitusUser($litusUser, $accessToken);
             })
         );
     }
