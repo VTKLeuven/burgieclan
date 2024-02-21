5bfb90ed8b7c0148a413a65308534587f805e65a
Robbe Serry <robbeserry@gmail.com>
2024-02-21 14:55:01 +0000

    BUR-42 add admin controllers

--- src/Entity/Module.php
+++ src/Entity/Module.php
@@ -30,6 +30,11 @@ class Module
         $this->courses = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -26,6 +26,11 @@ class Program
         $this->modules = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
