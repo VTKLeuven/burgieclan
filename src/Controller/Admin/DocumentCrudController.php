b4cf1ada64b25c2e9fe8c4e5ff44366b30e7605a
Benjamin Droogmans <benjamin.droogmans@gmail.com>
2024-03-02 21:29:24 +0000

    BUR-18 feat: added views for adding and editing Documets

---
+++ src/Controller/Admin/DocumentCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Document;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class DocumentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Document::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        
+        yield TextField::new('name');
+        yield DateTimeField::new('createDate')
+        ->hideOnForm();
+        yield DateTimeField::new('updateDate')
+        ->hideOnForm();
+        yield BooleanField::new('under_review');
+        yield AssociationField::new('category')
+        ->autocomplete();
+        yield AssociationField::new('course')
+        ->autocomplete();
+
+    }
+    
+}
