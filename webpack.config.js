ef8c275709ec7d45ab8d8e4d57240b884478cc57
stijn kuppens <stijn_kuppens@hotmail.com>
2024-03-16 11:17:16 +0100

    BUR-32 Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,6 @@ Encore
     .addEntry('admin', './assets/admin.js')
     .addEntry('search', './assets/search.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
