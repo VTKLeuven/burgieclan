142fecb0827af6a5c62718a64f0c99f9e099bfd9
Benjamin Droogmans <benjamin.droogmans@gmail.com>
2024-03-20 16:16:50 +0000

    BUR-18 removed add button

--- src/Controller/Admin/DocumentCrudController.php
+++ src/Controller/Admin/DocumentCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Document;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -14,6 +17,13 @@ class DocumentCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Document::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->disable(Action::NEW);
+
     }
 
     public function configureFields(string $pageName): iterable
