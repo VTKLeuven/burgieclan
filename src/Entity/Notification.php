a8b1da5a50348ef36f4dc5fff8a72ed11371f2cf
Annemerel <annemerelbobbaers@gmail.com>
2024-03-20 15:00:38 +0000

    Bur-52 comments deleted

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Entity;
 
-use App\Repository\NotificationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
