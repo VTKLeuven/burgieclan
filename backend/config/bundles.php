c2bd0a8f95a725e1728ef462e10c321b0812ee1e
Robbe Serry <robbeserry@gmail.com>
2024-04-07 16:04:29 +0200

    BUR-89 Install foundry to generate demo data

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -23,4 +23,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfonycasts\MicroMapper\SymfonycastsMicroMapperBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
