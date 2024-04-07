d94825de69e397bfbacd9bc72e776ad236d248f0
Robbe Serry <robbeserry@gmail.com>
2024-04-07 15:50:55 +0200

    BUR-89 Add CourseApi, mapper and general processors

---
+++ backend/src/ApiResource/CourseApi.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Entity\Course;
+use App\State\EntityClassDtoStateProcessor;
+use App\State\EntityClassDtoStateProvider;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    shortName: 'Course',
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ],
+    provider: EntityClassDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
+    stateOptions: new Options(entityClass: Course::class),
+)]
+class CourseApi
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    public ?int $id = null;
+
+    #[Assert\NotBlank]
+    public ?string $name = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(6)]
+    public ?string $code = null;
+
+    public ?array $professors = null;
+
+    public ?array $semesters = null;
+
+    #[Assert\Positive]
+    public ?int $credits = null;
+
+    public array $oldCourses;
+
+    public array $newCourses;
+
+    public array $modules;
+
+    public array $courseComments;
+}
