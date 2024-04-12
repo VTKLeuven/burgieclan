1789224df07ed6317712d78c63ea4c3e9911f606
Robbe Serry <robbeserry@gmail.com>
2024-04-12 14:47:48 +0200

    BUR-97 Create ModuleApi and tests

---
+++ backend/src/ApiResource/ModuleApi.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Entity\Module;
+use App\State\EntityClassDtoStateProcessor;
+use App\State\EntityClassDtoStateProvider;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    shortName: 'Module',
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ],
+    provider: EntityClassDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
+    stateOptions: new Options(entityClass: Module::class),
+)]
+class ModuleApi
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    public ?int $id = null;
+
+    #[Assert\NotBlank]
+    #[ApiFilter(SearchFilter::class, strategy: 'ipartial')]
+    public ?string $name = null;
+
+    public array $courses = [];
+
+    /*
+     * TODO: Add this code when corresponding dto's are available
+    public ProgramApi $program;
+    */
+}
