700e22ceeb2e174da2a3adac7d7066043c69e935
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2024-04-12 13:07:57 +0000

    BUR-127 tailwind css setup

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,12 +1,11 @@
-import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
 export default config;
