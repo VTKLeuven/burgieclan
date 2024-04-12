523f1f3688fb64e5a1ba0cdd3d6baf0ed5aa1e42
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2024-04-12 18:37:45 +0000

    BUR-108 create own collapsible component

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import Collapsible from "react-collapsible";
+import CollapsibleComponent from "@/components/collapsible/CollapsibleComponent";
 
 export default function LoginForm() {
     return (
@@ -16,12 +17,14 @@ export default function LoginForm() {
           */}
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
-                        alt="Your Company"
+                    <Image
+                        src="/images/logos/seafile-logo.png"
+                        alt="Burgieclan Logo"
+                        width={100}
+                        height={100}
+                        className="mx-auto"
                     />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-vtk-blue-500">
                         Sign in to your account
                     </h2>
                 </div>
@@ -35,77 +38,72 @@ export default function LoginForm() {
                             <Image
                                 src="/images/logos/vtk-logo-blue.png"
                                 alt="VTK Logo"
-                                width={60}
-                                height={30}
-                                className="p-2"
+                                width={50}
+                                height={25}
+                                className="p-2 pb-3"
                             />
-                            <p className="inline p-2">Log in with VTK</p>
+                            <p className="inline p-2 text-vtk-blue-500">Log in with VTK</p>
                         </button>
 
-                        <Collapsible
+                        <CollapsibleComponent
                             trigger= {
                                 <div
-                                    className="mt-10 font-semibold text-center text-sm leading-6 text-blue-600 hover:text-blue-500">
+                                    className="mt-10 font-semibold text-center text-sm leading-6 text-vtk-blue-500 hover:text-vtk-blue-400">
                                     Or log in manually >
                                 </div>
                             }
-                            triggerWhenOpen ={
-                                <div
-                                    className="font-semibold text-center text-sm leading-6 text-blue-600 hover:text-blue-500">
-                                    Or log in manually V
-                                </div>
-                            }
-                            transitionTime = {0.01}
-                        >
-                            <div>
-                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Email address
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        autoComplete="email"
-                                        required
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
+                            content={
+                                <div>
+                                    <div>
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Email address
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="email"
+                                                name="email"
+                                                type="email"
+                                                autoComplete="email"
+                                                required
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
 
-                            <div>
-                                <div className="flex items-center justify-between">
-                                    <label htmlFor="password"
-                                           className="block text-sm font-medium leading-6 text-gray-900">
-                                        Password
-                                    </label>
-                                    <div className="text-sm">
-                                        <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
-                                            Forgot password?
-                                        </a>
+                                    <div>
+                                        <div className="flex items-center justify-between">
+                                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Password
+                                            </label>
+                                            <div className="text-sm">
+                                                <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
+                                                    Forgot password?
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className="mt-2">
+                                            <input
+                                                id="password"
+                                                name="password"
+                                                type="password"
+                                                autoComplete="current-password"
+                                                required
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <button
+                                            type="submit"
+                                            className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                        >
+                                            Sign in
+                                        </button>
                                     </div>
                                 </div>
-                                <div className="mt-2">
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        autoComplete="current-password"
-                                        required
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
-
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                                >
-                                    Sign in
-                                </button>
-                            </div>
-                        </Collapsible>
+                            }
+                        />
                     </form>
                 </div>
             </div>
