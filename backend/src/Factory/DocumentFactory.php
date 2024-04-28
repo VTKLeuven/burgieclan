6d24e3d6e42c331951830ade152f0412e802d589
Robbe Serry <robbeserry@gmail.com>
2024-04-28 11:07:40 +0200

    BUR-102 added DocumentCommentApi factories and tests

---
+++ backend/src/Factory/DocumentFactory.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Factory;
+
+use App\Entity\Document;
+use App\Repository\DocumentRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<Document>
+ *
+ * @method        Document|Proxy                     create(array|callable $attributes = [])
+ * @method static Document|Proxy                     createOne(array $attributes = [])
+ * @method static Document|Proxy                     find(object|array|mixed $criteria)
+ * @method static Document|Proxy                     findOrCreate(array $attributes)
+ * @method static Document|Proxy                     first(string $sortedField = 'id')
+ * @method static Document|Proxy                     last(string $sortedField = 'id')
+ * @method static Document|Proxy                     random(array $attributes = [])
+ * @method static Document|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static DocumentRepository|RepositoryProxy repository()
+ * @method static Document[]|Proxy[]                 all()
+ * @method static Document[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Document[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Document[]|Proxy[]                 findBy(array $attributes)
+ * @method static Document[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Document[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ *
+ * @phpstan-method        Proxy<Document> create(array|callable $attributes = [])
+ * @phpstan-method static Proxy<Document> createOne(array $attributes = [])
+ * @phpstan-method static Proxy<Document> find(object|array|mixed $criteria)
+ * @phpstan-method static Proxy<Document> findOrCreate(array $attributes)
+ * @phpstan-method static Proxy<Document> first(string $sortedField = 'id')
+ * @phpstan-method static Proxy<Document> last(string $sortedField = 'id')
+ * @phpstan-method static Proxy<Document> random(array $attributes = [])
+ * @phpstan-method static Proxy<Document> randomOrCreate(array $attributes = [])
+ * @phpstan-method static RepositoryProxy<Document> repository()
+ * @phpstan-method static list<Proxy<Document>> all()
+ * @phpstan-method static list<Proxy<Document>> createMany(int $number, array|callable $attributes = [])
+ * @phpstan-method static list<Proxy<Document>> createSequence(iterable|callable $sequence)
+ * @phpstan-method static list<Proxy<Document>> findBy(array $attributes)
+ * @phpstan-method static list<Proxy<Document>> randomRange(int $min, int $max, array $attributes = [])
+ * @phpstan-method static list<Proxy<Document>> randomSet(int $number, array $attributes = [])
+ */
+final class DocumentFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'category' => DocumentCategoryFactory::randomOrCreate(),
+            'course' => CourseFactory::randomOrCreate(),
+            'name' => self::faker()->word(),
+            'under_review' => self::faker()->boolean(),
+            'user' => UserFactory::randomOrCreate(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Document $document): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Document::class;
+    }
+}
---
+++ backend/src/Mapper/DocumentCommentEntityToApiMapper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mapper;
+
+use App\ApiResource\DocumentCommentApi;
+use App\ApiResource\UserApi;
+use App\Entity\DocumentComment;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+#[AsMapper(from: DocumentComment::class, to: DocumentCommentApi::class)]
+class DocumentCommentEntityToApiMapper implements MapperInterface
+{
+    public function __construct(
+        private readonly MicroMapperInterface $microMapper,
+    ) {
+    }
+
+    public function load(object $from, string $toClass, array $context): object
+    {
+        assert($from instanceof DocumentComment);
+
+        $dto = new DocumentCommentApi();
+        $dto->id = $from->getId();
+
+        return $dto;
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        assert($from instanceof DocumentComment);
+        assert($to instanceof DocumentCommentApi);
+
+        $to->content = $from->getContent();
+        $to->anonymous = $from->isAnonymous();
+//        TODO when DocumentApi exists
+//        $to->document = $this->microMapper->map($from->getDocument(), DocumentApi::class, [
+//            MicroMapperInterface::MAX_DEPTH => 0,
+//        ]);
+        $to->creator = $this->microMapper->map($from->getUser(), UserApi::class, [
+            MicroMapperInterface::MAX_DEPTH => 0,
+        ]);
+        $to->createdAt = $from->getCreateDate()->format('Y-m-d H:i:s');
+        $to->updatedAt = $from->getUpdateDate()->format('Y-m-d H:i:s');
+
+        return $to;
+    }
+}
