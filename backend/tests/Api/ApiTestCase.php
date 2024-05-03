e24a722195bebcdc5d01ac6f01df8f67a2ec1016
Robbe Serry <robbeserry@gmail.com>
2024-05-03 12:06:51 +0200

    BUR-107 change tests to use token

--- backend/tests/Api/ApiTestCase.php
+++ backend/tests/Api/ApiTestCase.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api;
 
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\HttpOptions;
 use Zenstruck\Browser\KernelBrowser;
@@ -13,9 +14,25 @@ abstract class ApiTestCase extends KernelTestCase
         browser as baseKernelBrowser;
     }
 
+    protected string $token;
+
     protected function browser(array $options = [], array $server = []): KernelBrowser
     {
         return $this->baseKernelBrowser($options, $server)
             ->setDefaultHttpOptions(HttpOptions::create()->withHeader('Accept', 'application/ld+json'));
+    }
+
+    protected function setUp(): void
+    {
+        $user = UserFactory::createOne(['plainPassword' => 'password']);
+
+        $tokenResponse = $this->browser()
+            ->post('/api/auth/login', HttpOptions::json([
+                'username' => $user->getUsername(),
+                'password' => 'password',
+            ]))
+            ->json()
+            ->decoded();
+        $this->token = $tokenResponse['token'];
     }
 }
