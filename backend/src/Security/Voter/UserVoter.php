49d50fced52ced98c32d5243679c80fff47ecba1
Robbe Serry <robbeserry@gmail.com>
2024-05-17 14:43:59 +0200

    BUR-138 Return private userinfo only when user itself asks

---
+++ backend/src/Security/Voter/UserVoter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\ApiResource\UserApi;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+class UserVoter extends Voter
+{
+    public const VIEW_USERNAME = 'VIEW_USERNAME';
+    public const VIEW_EMAIL = 'VIEW_EMAIL';
+
+    public function __construct(
+        private readonly MicroMapperInterface $microMapper,
+    ) {
+    }
+
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::VIEW_EMAIL]) && $subject instanceof UserApi;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $currentUser = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$currentUser instanceof UserInterface) {
+            return false;
+        }
+
+        assert($subject instanceof UserApi);
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::VIEW_USERNAME:
+            case self::VIEW_EMAIL:
+                $requestedUser = $this->microMapper->map($subject, User::class, [
+                    MicroMapperInterface::MAX_DEPTH => 0,
+                ]);
+
+                if ($requestedUser == $currentUser) {
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+}
