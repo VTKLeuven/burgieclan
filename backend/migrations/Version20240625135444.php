dd6bb831c919581b0442c01a95db70af3e717aca
Robbe Serry <robbeserry@gmail.com>
2024-06-25 17:21:00 +0200

    BUR-77 Add page entity

---
+++ backend/migrations/Version20240625135444.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240625135444 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE page (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, url_key VARCHAR(255) NOT NULL, public_available TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE course_comment CHANGE creator_id creator_id INT NOT NULL');
+        $this->addSql('ALTER TABLE document CHANGE creator_id creator_id INT NOT NULL');
+        $this->addSql('ALTER TABLE document_comment CHANGE creator_id creator_id INT NOT NULL');
+        $this->addSql('ALTER TABLE notification CHANGE creator_id creator_id INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE page');
+        $this->addSql('ALTER TABLE document_comment CHANGE creator_id creator_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE document CHANGE creator_id creator_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE course_comment CHANGE creator_id creator_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE notification CHANGE creator_id creator_id INT DEFAULT NULL');
+    }
+}
