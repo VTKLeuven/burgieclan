1a15c7bd84af47f6392a54b67df6bdbbe0d489e9
Robbe Serry <robbeserry@gmail.com>
2024-06-25 17:21:20 +0200

    BUR-77 Add page crud controller

---
+++ backend/src/Controller/Admin/PageCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Page;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PageCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Page::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield Textfield::new('name');
+        yield Textfield::new('urlKey');
+        yield BooleanField::new('publicAvailable')
+            ->renderAsSwitch(false);
+        yield TextEditorField::new('content')
+            ->setTemplatePath('admin/text_editor.html.twig');
+    }
+}
