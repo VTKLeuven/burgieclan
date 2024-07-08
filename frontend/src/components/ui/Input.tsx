55ba309d654ca12914710d91b76667d006dec8d2
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2024-07-08 23:02:31 +0000

    BUR-43 add search keyboard shortcut

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,6 +1,16 @@
-export default function Input({id, name, type, placeholder} : {id?: string, name?: string, type?: string, placeholder: string}) {
-    return (
+import React, { forwardRef } from 'react';
+
+type InputProps = {
+    id?: string;
+    name?: string;
+    type?: string;
+    placeholder: string;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ id, name, type, placeholder }, ref) => (
         <input
+            ref={ref}
             id={id}
             name={name}
             type={type}
@@ -8,4 +18,8 @@ export default function Input({id, name, type, placeholder} : {id?: string, name
             className="block w-full max-h-8 rounded-sm border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
     )
-}
+);
+
+Input.displayName = 'Input';
+
+export default Input;
