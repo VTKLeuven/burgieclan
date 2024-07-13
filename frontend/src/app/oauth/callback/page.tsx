bfda26bc28aeca5d5ff21421e688b6d5fd00dcb6
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-07-13 23:04:04 +0200

    BUR-164 working oauth

--- frontend/src/app/oauth/callback/page.tsx
+++ frontend/src/app/oauth/callback/page.tsx
@@ -1,19 +1,22 @@
 'use client';
 
-import { useEffect } from 'react';
+import {useEffect, useRef} from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import axios from 'axios';
+import {NextResponse} from "next/server";
 
 const Callback = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
+    const hasRun = useRef(false);
 
     useEffect(() => {
-        console.log("callback");
+        if (hasRun.current) return;
+        hasRun.current = true;
 
-        const state = searchParams.get('state');
-        const storedState = localStorage.getItem('oauth_state');
-
+        // const state = searchParams.get('state');
+        // const storedState = localStorage.getItem('oauth_state');
+        //
         // if (state !== storedState) {
         //     console.error('State mismatch: potential CSRF attack.');
         //     return;
@@ -21,34 +24,40 @@ const Callback = () => {
 
         const code = searchParams.get('code');
         const codeVerifier = localStorage.getItem('code_verifier');
-        const tokenUrl = process.env.NEXT_PUBLIC_LITUS_OAUTH_TOKEN;
+        if (!codeVerifier) {
+            console.error('Code verifier is missing.');
+            return;
+        }
 
-        if (!tokenUrl) {
+        const exchangeTokenUrl = '/api/oauth/exchange-access-token';
+        if (!exchangeTokenUrl) {
             throw new Error("Missing environment variables for OAuth flow");
         }
 
-        console.log("code: ", code);
-        console.log("codeVerifier: ", codeVerifier);
-
         if (code && codeVerifier) {
-            console.log("code verified");
-            axios.post(tokenUrl, {
+
+            axios.post(exchangeTokenUrl, {
                 grant_type: 'authorization_code',
                 code: code,
                 client_id: process.env.NEXT_PUBLIC_LITUS_API_KEY,
                 redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL,
                 code_verifier: codeVerifier,
             })
-                .then((response: { data: { accessToken: any; }; }) => {
-                    const { accessToken } = response.data;
-                    console.log("access_token: ", accessToken);
+                .then(async (response) => {
+                    const {access_token} = response.data;
 
-                    return axios.post(process.env.NEXT_PUBLIC_BACKEND_AUTH, { accessToken });
+                    return await axios.post(process.env.NEXT_PUBLIC_BACKEND_AUTH, {
+                        accessToken: access_token
+                    }, {
+                        headers: {
+                            'accept': 'application/ld+json',
+                            'Content-Type': 'application/ld+json'
+                        }
+                    });
                 })
-                .then((response: { data: { jwt: any; }; }) => {
-                    document.cookie = `jwt=${response.data.jwt}; path=/; HttpOnly; Secure`;
-
-                    console.log("cookie: ", document.cookie);
+                .then((response) => {
+                    document.cookie = `jwt=${response.data.token}; path=/; HttpOnly; Secure`;
+                    console.log("jwt: ", response.data.token);
                     router.push('/');
                 })
                 .catch((error: any) => {
