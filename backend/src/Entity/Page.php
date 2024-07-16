3c2aa421899037ef7e2712c2fadd4eefdd23e3bb
Robbe Serry <robbeserry@gmail.com>
2024-07-16 19:45:04 +0200

    BUR-77 make url-key unique

--- backend/src/Entity/Page.php
+++ backend/src/Entity/Page.php
@@ -20,7 +20,7 @@ class Page
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $content = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private string $urlKey;
 
     #[ORM\Column]
