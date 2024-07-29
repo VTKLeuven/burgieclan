4550ba32fa9f29e88a3cb835b8fbae9aa85989ba
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-07-29 17:35:50 +0200

    BUR-169 add manual jwt entry at /login/jwt/[jwt]

--- frontend/src/utils/api.tsx
+++ frontend/src/utils/api.tsx
@@ -1,11 +1,20 @@
 /**
  * API Client for requests to the backend server.
  */
+<<<<<<< Updated upstream
 export const apiClient = async (method: string, endpoint: string, body?: any, headers?: Record<string, string>) => {
     const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
     if (!baseUrl) {
         throw new Error(`Missing environment variable for backend base URL`)
+=======
+export const ApiClient = async (method: string, endpoint: string, body?: any, headers?: Record<string, string>) => {
+    const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+    const frontendBaseUrl = process.env.NEXT_PUBLIC_FRONTEND_URL;
+
+    if (!backendBaseUrl || !frontendBaseUrl) {
+        throw new Error(`Missing environment variable for backend or frontend base URL`)
+>>>>>>> Stashed changes
     }
 
     const url = baseUrl + endpoint;
