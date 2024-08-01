1d78028b5151b1ec043c2e7060adf68f981c66ca
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-01 13:39:53 +0200

    BUR-0 hotfix in test: add suspense to oauth callback

--- frontend/src/app/oauth/callback/page.tsx
+++ frontend/src/app/oauth/callback/page.tsx
@@ -1,4 +1,12 @@
-'use client';
-import {LitusOAuthCallback} from "@/components/login/LitusOAuthHelper";
+'use client'
 
-export default LitusOAuthCallback;
+import { LitusOAuthCallback } from "@/components/login/LitusOAuthHelper";
+import {Suspense} from "react";
+
+export default function OAuthCallbackPage() {
+    return (
+        <Suspense>
+            <LitusOAuthCallback />
+        </Suspense>
+    );
+}
