9b641ef4b451942834da3913712a6cbc93d1d6d9
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-02 14:57:26 +0200

    BUR-0: fix middleware config

--- frontend/src/app/middleware.js
+++ frontend/src/app/middleware.js
@@ -6,7 +6,7 @@ export function middleware() {
 
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Origin', 'https://dev.burgieclan.vtk.be')
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
