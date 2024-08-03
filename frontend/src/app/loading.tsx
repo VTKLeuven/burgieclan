435f5ec4f66e952086a1fa11f33a37553d0f51cf
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-03 20:16:12 +0200

    BUR-163 add loading skeleton and suspense

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,6 @@
+import LoadingSkeleton from "@/components/common/LoadingSkeleton";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
+}
