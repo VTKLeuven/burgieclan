6e3009cd440be7d35959d4ea40fa20514794ee02
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-03 20:47:14 +0200

    BUR-164 add loading page & apply to oauth

--- frontend/src/app/oauth/callback/page.tsx
+++ frontend/src/app/oauth/callback/page.tsx
@@ -2,14 +2,17 @@
 
 import {LitusOAuthCallback} from "@/utils/oauth";
 import {Suspense} from "react";
-import Loading from "@/app/loading";
+import LoadingPage from "@/components/common/LoadingPage";
 
 export default function OAuthCallbackPage() {
     // Wrap useSearchParams() from LitusOAuthCallback in Suspense:
     // https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
     return (
-        <Suspense fallback={ <Loading /> }>
-            <LitusOAuthCallback />
-        </Suspense>
+        <>
+            <Suspense>
+                <LoadingPage />
+                <LitusOAuthCallback />
+            </Suspense>
+        </>
     );
 }
