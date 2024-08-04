2aad41ef0230f8e10eeb937eb62a9df31b47ff67
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-04 15:48:43 +0200

    BUR-163 code cleanup

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,6 +1,5 @@
 import LoadingPage from "@/components/common/LoadingPage";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
     return <LoadingPage />
 }
