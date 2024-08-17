1a9306f07766969294550a74fc364e3a90dbaf64
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-17 18:56:13 +0200

    BUR-110 dynamically generated error pages

---
+++ frontend/src/components/error/ErrorPage.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/navigation";
+
+export default function ErrorPage(status: string, title: string, detail: string) {
+    const router = useRouter();
+
+    const redirectHome = () => {
+        router.push('/');
+    };
+
+    return (
+        <>
+            <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                <div className="text-center">
+                    <p className="text-base font-semibold text-amber-700">{ status }</p>
+                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{ title }</h1>
+                    { status == "404" ? <p className="mt-6 text-base leading-7 text-gray-600"> Sorry, we couldn’t find the page you’re looking for. </p> : <></> }
+                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <button
+                            type="button"
+                            onClick={redirectHome}
+                            className="primary-button">
+                            Go back home
+                        </button>
+                        <button
+                            type="button"
+                            onClick={redirectHome}
+                            className="white-button">
+                            Contact support <span aria-hidden="true">&rarr;</span>
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
