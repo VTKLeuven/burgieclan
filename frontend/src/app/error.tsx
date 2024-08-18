9b52221cee969ab54dec6405c15c277d632e5d63
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-18 16:24:08 +0200

    BUR-110 tmp test error boundary

---
+++ frontend/src/app/error.tsx
@@ -0,0 +1,28 @@
+'use client' // Error boundaries must be Client Components
+
+import { useEffect } from 'react'
+import * as Sentry from "@sentry/nextjs";
+
+export default function Error({ error, reset, } : {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        Sentry.captureException(error);
+        console.error(error)
+    }, [error])
+
+    return (
+        <div>
+            <h2>Something went wrong!</h2>
+            <button
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </div>
+    )
+}
