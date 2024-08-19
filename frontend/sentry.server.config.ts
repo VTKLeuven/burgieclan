943b7af552005e9b1eca35e9d175d434fa4c9970
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-19 12:07:30 +0200

    BUR-183 change conditional sentry init

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -4,19 +4,24 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://c7a1562dd406594aeb3e6d27b13e53bc@o918793.ingest.us.sentry.io/4506733883621376",
+/**
+ * Only initialize Sentry in production or test environments, not in development.
+ */
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
+  Sentry.init({
+    dsn: "https://c7a1562dd406594aeb3e6d27b13e53bc@o918793.ingest.us.sentry.io/4506733883621376",
 
-  // Only enable Sentry in production environment
-  enabled: process.env.NODE_ENV === 'production',
+    // Only enable Sentry in production environment
+    environment: "frontend-" + process.env.NODE_ENV,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
-});
+    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
+
+  });
+}
