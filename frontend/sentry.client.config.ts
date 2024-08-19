e21b07a54a50e95d51fab4b2b6f6718dcb194ea0
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-08-19 12:10:42 +0200

    BUR-183 code comments

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -4,20 +4,18 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-/**
- * Only initialize Sentry in production or test environments, not in development.
- */
+// Conditionally load Sentry based on the environment
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
   Sentry.init({
     dsn: "https://c7a1562dd406594aeb3e6d27b13e53bc@o918793.ingest.us.sentry.io/4506733883621376",
 
-    // Only enable Sentry in production environment
+    // Set Sentry environment
     environment: "frontend-" + process.env.NODE_ENV,
 
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    // Setting this option to true will print useful information to the console while you're setting up Sentry
     debug: false,
     replaysOnErrorSampleRate: 1.0,
 
