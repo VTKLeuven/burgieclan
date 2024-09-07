4eec45f26da3bd45dca5e238e40a847729e1b283
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-09-07 14:17:06 +0200

    BUR-39 WIP add lexical editor

---
+++ frontend/src/app/(header-footer)/editor/page.tsx
@@ -0,0 +1,11 @@
+import Editor from "@/components/editor/Editor";
+
+const EditorPage = () => {
+    return (
+        <div>
+            <Editor />
+        </div>
+    )
+}
+
+export default EditorPage;
---
+++ frontend/src/components/editor/plugins/TreeViewPlugin.tsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import styles from '../editor.module.css';
+
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {TreeView} from '@lexical/react/LexicalTreeView';
+
+export default function TreeViewPlugin(): JSX.Element {
+    const [editor] = useLexicalComposerContext();
+    return (
+        <TreeView
+            viewClassName={styles.treeViewOutput}
+            treeTypeButtonClassName={styles.debugTreetypeButton}
+            timeTravelPanelClassName={styles.debugTimetravelPanel}
+            timeTravelButtonClassName={styles.debugTimetravelButton}
+            timeTravelPanelSliderClassName={styles.debugTimetravelPanelSlider}
+            timeTravelPanelButtonClassName={styles.debugTimetravelPanelButton}
+            editor={editor}
+        />
+    );
+}
+
