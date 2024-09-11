a8bd4116c8e3d611354b75ceb82a48c20487f796
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-09-11 11:12:26 +0200

    BUR-39 math & active styling

--- frontend/src/components/editor/Editor.tsx
+++ frontend/src/components/editor/Editor.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import 'katex/dist/katex.min.css'
 import {useEditor, EditorContent, useCurrentEditor, EditorProvider} from '@tiptap/react'
+import { TextSelection } from '@tiptap/pm/state'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
@@ -9,6 +11,7 @@ import {StarterKit} from "@tiptap/starter-kit";
 import {Color} from "@tiptap/extension-color";
 import {TextStyle} from "@tiptap/extension-text-style";
 import {ListItem} from "@tiptap/extension-list-item";
+import { Mathematics } from '@tiptap-pro/extension-mathematics'
 import {
     Bold,
     Code,
@@ -20,7 +23,8 @@ import {
     Minus,
     Redo2,
     Strikethrough, TextQuote,
-    Undo2
+    Undo2,
+    SquareFunction
 } from "lucide-react";
 import {useState} from "react";
 
@@ -30,7 +34,7 @@ const MenuBar = ({ editor }) => {
     }
 
     return (
-        <div className="tiptap flex border border-gray-900/10 rounded-t-md p-2 divide-x items-center">
+        <div className="tiptap flex border border-gray-900/10 rounded-t-md p-2 items-center">
             <button
                 onClick={() => editor?.chain().focus().toggleBold().run()}
                 disabled={
@@ -40,7 +44,7 @@ const MenuBar = ({ editor }) => {
                         .toggleBold()
                         .run()
                 }
-                className={`${editor.isActive('bold') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('bold') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Bold className="w-5 h-5 text-gray-900"/>
             </button>
@@ -53,7 +57,7 @@ const MenuBar = ({ editor }) => {
                         .toggleItalic()
                         .run()
                 }
-                className={`${editor.isActive('italic') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('italic') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Italic className="w-5 h-5 text-gray-900"/>
             </button>
@@ -66,7 +70,7 @@ const MenuBar = ({ editor }) => {
                         .toggleStrike()
                         .run()
                 }
-                className={`${editor.isActive('strike') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('strike') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Strikethrough className="w-5 h-5 text-gray-900"/>
             </button>
@@ -79,7 +83,7 @@ const MenuBar = ({ editor }) => {
                         .toggleCode()
                         .run()
                 }
-                className={`${editor.isActive('code') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('code') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Code className="w-5 h-5 text-gray-900"/>
             </button>
@@ -91,67 +95,67 @@ const MenuBar = ({ editor }) => {
             {/*</button>*/}
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().setParagraph().run()}*/}
-            {/*    className={editor.isActive('paragraph') ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('paragraph') ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    Paragraph*/}
             {/*</button>*/}
             <button
-                onClick={() => editor?.chain().focus().toggleHeading({ level: 1 }).run()}
-                className={`${editor.isActive('heading', { level: 1 }) ? 'is-active' : ''} px-3`}
+                onClick={() => editor?.chain().focus().toggleHeading({level: 1}).run()}
+                className={`${editor.isActive('heading', {level: 1}) ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Heading1 className="w-5 h-5 text-gray-900"/>
             </button>
             <button
-                onClick={() => editor?.chain().focus().toggleHeading({ level: 2 }).run()}
-                className={`${editor.isActive('heading', { level: 2 }) ? 'is-active' : ''} px-3`}
+                onClick={() => editor?.chain().focus().toggleHeading({level: 2}).run()}
+                className={`${editor.isActive('heading', {level: 2}) ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <Heading2 className="w-5 h-5 text-gray-900"/>
             </button>
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleHeading({ level: 3 }).run()}*/}
-            {/*    className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('heading', { level: 3 }) ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <Heading3 className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleHeading({ level: 4 }).run()}*/}
-            {/*    className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('heading', { level: 4 }) ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <Heading4 className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleHeading({ level: 5 }).run()}*/}
-            {/*    className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('heading', { level: 5 }) ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <Heading5 className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleHeading({ level: 6 }).run()}*/}
-            {/*    className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('heading', { level: 6 }) ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <Heading6 className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
             <button
                 onClick={() => editor?.chain().focus().toggleBulletList().run()}
-                className={`${editor.isActive('bulletList') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('bulletList') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <List className="w-5 h-5 text-gray-900"/>
             </button>
             <button
                 onClick={() => editor?.chain().focus().toggleOrderedList().run()}
-                className={`${editor.isActive('orderedList') ? 'is-active' : ''} px-3`}
+                className={`${editor.isActive('orderedList') ? 'isActive' : ''} p-1 mx-2 rounded-md`}
             >
                 <ListOrdered className="w-5 h-5 text-gray-900"/>
             </button>
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleCodeBlock().run()}*/}
-            {/*    className={editor.isActive('codeBlock') ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('codeBlock') ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <Code className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
             {/*<button*/}
             {/*    onClick={() => editor?.chain().focus().toggleBlockquote().run()}*/}
-            {/*    className={editor.isActive('blockquote') ? 'is-active' : ''}*/}
+            {/*    className={editor.isActive('blockquote') ? 'isActive' : ''}*/}
             {/*>*/}
             {/*    <TextQuote className="w-5 h-5 text-gray-900"/>*/}
             {/*</button>*/}
@@ -170,7 +174,7 @@ const MenuBar = ({ editor }) => {
                         .undo()
                         .run()
                 }
-                className="px-3"
+                className="p-1 mx-2 clickable"
             >
                 <Undo2 className="w-5 h-5 text-gray-900"/>
             </button>
@@ -183,9 +187,27 @@ const MenuBar = ({ editor }) => {
                         .redo()
                         .run()
                 }
-                className="pl-3"
+                className="p-1 mx-2 clickable"
             >
                 <Redo2 className="w-5 h-5 text-gray-900"/>
+            </button>
+            <button
+                // Clicking inserts a math block
+                onClick={() => {
+                    // Insert two dollar signs at the current cursor position
+                    editor.chain().focus().insertContent('$$').run();
+
+                    // Move the cursor between the dollar signs
+                    const { tr } = editor.state;
+                    const position = tr.selection.from - 1; // One character back (to move between the dollar signs)
+                    const newSelection = TextSelection.create(tr.doc, position);
+
+                    // Dispatch the transaction with the new cursor position
+                    editor.view.dispatch(tr.setSelection(newSelection));
+                }}
+                className="p-1 mx-2 clickable"
+            >
+                <SquareFunction className="w-5 h-5 text-gray-900"/>
             </button>
         </div>
     )
@@ -219,12 +241,20 @@ const Editor = () => {
             }),
             Color.configure({ types: [TextStyle.name, ListItem.name] }),
             TextStyle.configure({ types: [ListItem.name] }),
+            Mathematics,
         ],
         autofocus: true,
         editorProps: {
             attributes: {
                 class: 'tiptap overflow-auto mx-auto h-32 border-l border-r border-b border-gray-900/10 rounded-b-md p-3 focus:outline-none',
             },
+            // Strip styling from pasted content
+            handlePaste(view, event, slice) {
+                event.preventDefault()
+                const text = event.clipboardData.getData('text/plain')
+                view.dispatch(view.state.tr.insertText(text)) // insert plain text as a new paragraph in the editor
+                return true
+            }
         },
     })
 
