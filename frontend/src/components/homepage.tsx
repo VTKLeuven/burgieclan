c202c38b32c93b64515fbffbedf063013c5c0c7b
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-09-29 11:06:54 +0200

    BUR-197: Extracted sections as parameters

--- frontend/src/components/homepage.tsx
+++ frontend/src/components/homepage.tsx
@@ -1,11 +1,15 @@
 'use client'
 
-import Image from 'next/image'
-import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-import React, { useRef, useEffect, useState } from 'react';
-import LitusOAuthButton from "@/components/login/LitusOAuthButton";
+import React, { ReactNode } from 'react';
 
-export default function HomePage() {
+interface HomePageProps {
+    topLeft?: ReactNode;
+    topRight?: ReactNode;
+    bottomLeft?: ReactNode;
+    bottomRight?: ReactNode;
+}
+
+export default function HomePage({ topLeft, topRight, bottomLeft, bottomRight }: HomePageProps) {
 
     return (
         <div className="mt-10 flex justify-center">
@@ -19,27 +23,19 @@ export default function HomePage() {
                 <div className="grid grid-rows-[65%_35%] h-[90vh]">
                     <div className="grid grid-cols-2">
                         <div className="bg-red-200">
-                            <div className="pl-5 pt-5">
-                                <h3>Pick up where you left off</h3>
-                            </div>
+                            {topLeft}
                         </div>
                         <div className="bg-blue-200">
-                            <div className="flex items-center justify-center h-full">
-                                <h3>Drag & drop bestanden</h3>
-                            </div>
+                            {topRight}
                         </div>
                     </div>
 
                     <div className="grid grid-cols-[30%_70%]">
                         <div className="bg-green-200">
-                            <div className="pt-5">
-                                <h3>Nieuws</h3>
-                            </div>
+                            {bottomLeft}
                         </div>
                         <div className="bg-yellow-200">
-                            <div className="pt-5">
-                                <h3>Snel navigeren naar</h3>
-                            </div>
+                            {bottomRight}
                         </div>
                     </div>
                 </div>
