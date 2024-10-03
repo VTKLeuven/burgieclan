8c5e065b60c2059fb0500a536f72120b545e7cdf
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-10-04 00:08:14 +0200

    BUR-186 fix header based on auth

--- frontend/src/app/(header-footer)/layout.tsx
+++ frontend/src/app/(header-footer)/layout.tsx
@@ -1,31 +1,13 @@
-'use client'
-
 import "@/app/globals.css";
-import React, { useEffect, useState } from "react";
-import Header from "@/components/header/Header";
-import { hasJwt } from "@/actions/oauth";
+import React from "react";
 import Footer from "@/components/footer/Footer";
+import HeaderWrapper from "@/components/header/HeaderWrapper";
 
 export default function HeaderLayout({children,}: Readonly<{ children: React.ReactNode }>) {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            try {
-                const authenticated = await hasJwt();
-                setIsAuthenticated(authenticated);
-            } catch (error) {
-                throw Error('Error checking authentication status:', error);
-            }
-        };
-
-        checkAuthentication();
-    }, []);
-
     return (
         <>
             <div className="flex h-full flex-col min-h-full">
-                <Header isAuthenticated={isAuthenticated} />
+                <HeaderWrapper />
                 <div className="grow">
                     {children}
                 </div>
