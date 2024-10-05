ce22042b44a9eaf09b98e3fb708c34d351f795d8
Pedro Devogelaere <pedro.devogelaere@vtk.be>
2024-10-05 14:33:21 +0000

    BUR-192 Fixed frontend (but close)

--- frontend/src/app/(header-footer)/page.tsx
+++ frontend/src/app/(header-footer)/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { useState } from 'react';
+import UploadForm from '@/components/upload/UploadForm';
+
 export default function Homepage() {
+  const [isUploadFormOpen, setIsUploadFormOpen] = useState(false);
+
   return (
     <div>
-      Homepage
+      <button onClick={() => setIsUploadFormOpen(true)} className="primary-button">
+        Open Upload Form
+      </button>
+      <UploadForm isOpen={isUploadFormOpen} setIsOpen={setIsUploadFormOpen} />
     </div>
   );
 }
