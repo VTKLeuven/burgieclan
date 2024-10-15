08d89ccd2aaca082b16950c1f73ffc2445b2b454
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-10-15 20:21:45 +0200

    Revert "BUR-199 WIP document page api"
    This reverts commit a0b78596b7099cede7d57ae95476150ca432bbf3.

--- frontend/src/app/(header-footer)/document/[id]/page.tsx
+++ frontend/src/app/(header-footer)/document/[id]/page.tsx
@@ -1,30 +1,21 @@
 'use client'
 
 import {useEffect, useState} from "react";
+import {ApiClient, ApiClientError} from "@/utils/api";
 import ErrorPage from "@/components/error/ErrorPage";
 import Loading from "@/app/loading";
-import {ApiClient} from "@/actions/api";
-import {ApiError} from "@/utils/error/apiError";
 
 export default function DocumentPage({ params }: { params: any }) {
     const { id } = params;
 
     const [document, setDocument] = useState<any>(null);
-    const [error, setError] = useState<ApiError | null>(null);
+    const [error, setError] = useState<ApiClientError | null>(null);
 
     useEffect(() => {
         const FetchData = async () => {
             try {
-                const result = await ApiClient('GET', `/api/documents/${id}`, undefined,
-                    new Headers({ 'Preload': '/creator', 'Fields': '/creator/username' })
-                );
-                const creatorUsername = await ApiClient('GET', result.creator, undefined,
-                    new Headers({ 'Fields': '/username' })
-                );
-
+                const result = await ApiClient('GET', `/api/documents/${id}`);
                 console.log(result);
-                console.log(creatorUsername);
-
                 setDocument(result);
             } catch (err: any) {
                 setError(err);
