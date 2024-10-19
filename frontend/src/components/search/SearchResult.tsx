293560e685417f87b124401eb3ddfb9987ed16c2
Robbe Serry <robbeserry@gmail.com>
2024-10-20 00:51:26 +0200

    BUR-75 fix mapping and cleanup

--- frontend/src/components/search/SearchResult.tsx
+++ frontend/src/components/search/SearchResult.tsx
@@ -1,5 +1,6 @@
 import { ComboboxOption } from "@headlessui/react";
 import { Course, Document, Module, Program } from "@/utils/types";
+import clsx from "clsx";
 
 type SearchResultProps = {
     mainResult: string;
@@ -7,31 +8,36 @@ type SearchResultProps = {
     redirect: string;
 };
 
-export default function SearchResult ({mainResult, extraInfo, redirect}: SearchResultProps) {
+export default function SearchResult({ mainResult, extraInfo, redirect }: SearchResultProps) {
     return <ComboboxOption
         value={redirect}
-        className="cursor-default select-none px-4 py-2 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+        className="cursor-default select-none px-4 py-2 data-[focus]:bg-amber-600 data-[focus]:text-white"
     >
-        {mainResult}
-        {extraInfo}
+        {({ focus }) => (<div className="flex justify-between">
+            <span className="truncate">{mainResult}</span>
+            <span className={clsx('ml-2', focus && 'text-white', !focus && 'text-gray-500')}>
+                {extraInfo}
+            </span>
+        </div>
+        )}
     </ComboboxOption>
 }
 
-export function CourseSearchResult ({course}: { course: Course }) {
+export function CourseSearchResult({ course }: { course: Course }) {
     return <SearchResult mainResult={course.name} extraInfo={course.code}
-                         redirect={"/course/" + course.id}/>
+        redirect={"/course/" + course.id} />
 }
 
-export function ModuleSearchResult ({module}: { module: Module }) {
+export function ModuleSearchResult({ module }: { module: Module }) {
     return <SearchResult mainResult={module.name} extraInfo={module.program.name}
-                         redirect={"/module/" + module.id}/>
+        redirect={"/module/" + module.id} />
 }
 
-export function ProgramSearchResult ({program}: { program: Program }) {
-    return <SearchResult mainResult={program.name} redirect={"/program/" + program.id}/>
+export function ProgramSearchResult({ program }: { program: Program }) {
+    return <SearchResult mainResult={program.name} redirect={"/program/" + program.id} />
 }
 
-export function DocumentSearchResult ({document}: { document: Document }) {
+export function DocumentSearchResult({ document }: { document: Document }) {
     return <SearchResult mainResult={document.name} extraInfo={document.course.name}
-                         redirect={"/document/" + document.id}/>
+        redirect={"/document/" + document.id} />
 }
