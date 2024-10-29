60fb0e9b9b02461af67822dccca6d83768fb2b08
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-10-29 21:25:59 +0100

    BUR-199 code improvements

--- frontend/src/components/document/DocumentPreview.tsx
+++ frontend/src/components/document/DocumentPreview.tsx
@@ -1,5 +1,6 @@
-import {Calendar, CircleUser, File, Package, RefreshCcw} from "lucide-react";
-import UpvoteButton from "@/components/ui/UpvoteButton";
+import { Calendar, CircleUser, File, Package, RefreshCcw } from "lucide-react";
+import VoteButton from "@/components/ui/VoteButton";
+import DocumentInfoField from "@/components/document/DocumentInfoField";
 
 export default function DocumentPreview () {
     return (
@@ -13,20 +14,11 @@ export default function DocumentPreview () {
             {/* Description */}
             <div className="flex flex-row justify-between py-5">
                 <div className="flex space-x-8">
-                    <span className="inline-flex items-center space-x-2">
-                        <Calendar size={18}/>
-                        <div>2021-01-01</div>
-                    </span>
-                    <span className="inline-flex items-center space-x-2">
-                        <Package size={18}/>
-                        <div>4 MB</div>
-                    </span>
+                    <DocumentInfoField icon={Calendar} value="2021-01-01"/>
+                    <DocumentInfoField icon={Package} value="4 MB"/>
                 </div>
                 <div>
-                    <span className="inline-flex items-center space-x-2">
-                        <CircleUser size={18}/>
-                        <div>Dries Vanspauwen</div>
-                    </span>
+                    <DocumentInfoField icon={CircleUser} value={"Dries Vanspauwen"} />
                 </div>
             </div>
 
@@ -34,12 +26,8 @@ export default function DocumentPreview () {
             <div className="w-full border-t border-vtk-blue-600" />
 
             <div className="flex flex-row justify-between py-5 place-items-center">
-                <UpvoteButton/>
-                <span className="inline-flex items-center space-x-2 text-gray-500">
-                    <RefreshCcw size={18}/>
-                    <div>Last updated 20/10/2024 at 15:35</div>
-                </span>
-
+                <VoteButton/>
+                <DocumentInfoField className="text-gray-500" icon={RefreshCcw} value={"Last updated 20/10/2024 at 15:35"} />
             </div>
 
         </div>
