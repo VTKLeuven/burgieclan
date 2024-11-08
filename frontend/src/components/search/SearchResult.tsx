98bc7eec49a0bc2a52035e8fc0d7d184c8e880cb
Robbe Serry <robbeserry@gmail.com>
2024-11-08 23:06:10 +0100

    BUR-75 fix merge and other errors

--- frontend/src/components/search/SearchResult.tsx
+++ frontend/src/components/search/SearchResult.tsx
@@ -10,7 +10,7 @@ type SearchResultProps = {
 
 export default function SearchResult({ mainResult, extraInfo, redirect }: SearchResultProps) {
     return <ComboboxOption
-        value={redirect}
+        value={{ redirect }}
         className="cursor-default select-none px-4 py-2 data-[focus]:bg-amber-600 data-[focus]:text-white"
     >
         {({ focus }) => (<div className="flex justify-between">
