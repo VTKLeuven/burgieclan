929ed18eb4532cebf0f71463704e8e4b3c9297b3
Robbe Serry <robbeserry@gmail.com>
2024-11-09 02:21:46 +0100

    BUR-174 refactor zip controller and add openapi specs

--- backend/src/ApiResource/ZipApi.php
+++ backend/src/ApiResource/ZipApi.php
@@ -4,7 +4,9 @@ namespace App\ApiResource;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model;
 use App\Controller\Api\DownloadZipController;
+use ArrayObject;
 
 #[ApiResource(
     shortName: 'Download zip',
@@ -12,7 +14,51 @@ use App\Controller\Api\DownloadZipController;
         new Post(
             uriTemplate: 'zip',
             controller: DownloadZipController::class,
-            name: 'Download zip'
+            openapi: new Model\Operation(
+                responses: [
+                    '200' => [
+                        'description' => 'Download zip file',
+                        'content' => [
+                            'application/zip' => [
+                                'schema' => [
+                                    'type' => 'string',
+                                    'format' => 'binary',
+                                ],
+                            ],
+                        ],
+                    ],
+                    '204' => [
+                        'description' => 'No content to zip',
+                    ],
+                ],
+                requestBody: new Model\RequestBody(
+                    content: new ArrayObject([
+                        'application/ld+json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'programs' => [
+                                        'type' => 'array',
+                                        'format' => 'iri-reference',
+                                        'example' => ['/api/programs/1']
+                                    ],
+                                    'modules' => [
+                                        'type' => 'array',
+                                        'format' => 'iri-reference',
+                                        'example' => ['/api/modules/1']
+                                    ],
+                                    'courses' => [
+                                        'type' => 'array',
+                                        'format' => 'iri-reference',
+                                        'example' => ['/api/courses/1']
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ])
+                ),
+            ),
+            name: 'Download zip file',
         ),
     ]
 )]
