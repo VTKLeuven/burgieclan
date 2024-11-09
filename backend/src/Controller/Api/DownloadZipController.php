b5027110e912348acbc972b5979e9fa70f34fbbe
Robbe Serry <robbeserry@gmail.com>
2024-11-09 02:21:46 +0100

    BUR-174 refactor zip controller and add openapi specs

--- backend/src/Controller/Api/DownloadZipController.php
+++ backend/src/Controller/Api/DownloadZipController.php
@@ -27,107 +27,139 @@ final class DownloadZipController extends AbstractController
 
     public function __invoke(ZipApi $zipApi): Response
     {
-        $programs = [];
-        foreach ($zipApi->programs as $program) {
-            $programs[] = $this->microMapper->map($program, Program::class, [
-                MicroMapperInterface::MAX_DEPTH => 0,
-            ]);
-        }
-        $modules = [];
-        foreach ($zipApi->modules as $module) {
-            $modules[] = $this->microMapper->map($module, Module::class, [
-                MicroMapperInterface::MAX_DEPTH => 0,
-            ]);
-        }
-        $courses = [];
-        foreach ($zipApi->courses as $course) {
-            $courses[] = $this->microMapper->map($course, Course::class, [
-                MicroMapperInterface::MAX_DEPTH => 0,
-            ]);
-        }
+        $programs = $this->mapEntities($zipApi->programs, Program::class);
+        $modules = $this->mapEntities($zipApi->modules, Module::class);
+        $courses = $this->mapEntities($zipApi->courses, Course::class);
 
-        $zip = new ZipArchive();
-        $content = '';
-
-        if (!empty($programs)) {
-            foreach ($programs as $program) {
-                foreach ($program->getModules() as $module) {
-                    foreach ($module->getCourses() as $course) {
-                        foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                            $content .= $document->getFileName();
-                        }
-                    }
-                }
-            }
-        }
-        if (!empty($modules)) {
-            foreach ($modules as $module) {
-                foreach ($module->getCourses() as $course) {
-                    foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                        $content .= $document->getFileName();
-                    }
-                }
-            }
-        }
-        if (!empty($courses)) {
-            foreach ($courses as $course) {
-                foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                    $content .= $document->getFileName();
-                }
-            }
-        }
+        $content = $this->generateContentHash($programs, $modules, $courses);
 
         if (!empty($content)) {
-            $hash = md5($content);
-            $date = (new DateTime())->format('Y-m-d');
-            $file_name = sprintf('%s/data/exports/document-export-%s-%s.zip', $this->kernel->getProjectDir(), $date, $hash);
-
-            if (!file_exists($file_name) && $zip->open($file_name, ZipArchive::CREATE) === true) {
-                if (!empty($programs)) {
-                    foreach ($programs as $program) {
-                        $zip->addEmptyDir($program->getName());
-                        foreach ($program->getModules() as $module) {
-                            $zip->addEmptyDir($module->getName());
-                            foreach ($module->getCourses() as $course) {
-                                $zip->addEmptyDir($course->getName());
-                                foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                                    $path = $this->storage->resolvePath($document, 'file');
-                                    $zip->addFile($path, $program->getName() . '/' . $module->getName() . '/' . $course->getName() . '/' . $document->getFileName());
-                                }
-                            }
-                        }
-                    }
-                }
-                if (!empty($modules)) {
-                    foreach ($modules as $module) {
-                        $zip->addEmptyDir($module->getName());
-                        foreach ($module->getCourses() as $course) {
-                            $zip->addEmptyDir($course->getName());
-                            foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                                $path = $this->storage->resolvePath($document, 'file');
-                                $zip->addFile($path, $module->getName() . '/' . $course->getName() . '/' . $document->getFileName());
-                            }
-                        }
-                    }
-                }
-                if (!empty($courses)) {
-                    foreach ($courses as $course) {
-                        $zip->addEmptyDir($course->getName());
-                        foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
-                            $path = $this->storage->resolvePath($document, 'file');
-                            $zip->addFile($path, $course->getName() . '/' . $document->getFileName());
-                        }
-                    }
-                }
-            }
-
-            $response = new Response(file_get_contents($file_name));
-            $response->headers->set('Content-Type', 'application/zip');
-            $response->headers->set('Content-Disposition', 'attachment;filename="' . $file_name . '"');
-            $response->headers->set('Content-length', filesize($file_name));
-
-            return $response;
+            $fileName = $this->createZipFile($content, $programs, $modules, $courses);
+            return $this->createFileResponse($fileName);
         }
+
         return new Response('No content to zip', Response::HTTP_NO_CONTENT);
+    }
+
+    private function mapEntities(array $entities, string $class): array
+    {
+        return array_map(fn($entity) => $this->microMapper->map($entity, $class, [
+            MicroMapperInterface::MAX_DEPTH => 0,
+        ]), $entities);
+    }
+
+    /**
+     * @param Program[] $programs
+     * @param Module[]  $modules
+     * @param Course[]  $courses
+     */
+    private function generateContentHash(array $programs, array $modules, array $courses): string
+    {
+        $content = '';
+
+        foreach ($programs as $program) {
+            $content .= $this->getModuleContent($program->getModules()->toArray());
+        }
+
+        foreach ($modules as $module) {
+            $content .= $this->getModuleContent([$module]);
+        }
+
+        foreach ($courses as $course) {
+            $content .= $this->getCourseContent($course);
+        }
+
+        return md5($content);
+    }
+
+    /**
+     * @param Module[] $modules
+     * @return string
+     */
+    private function getModuleContent(array $modules): string
+    {
+        $content = '';
+
+        foreach ($modules as $module) {
+            $content .= $module->getName();
+            $content .= $this->getModuleContent($module->getModules()->toArray());
+            foreach ($module->getCourses()->toArray() as $course) {
+                $content .= $this->getCourseContent($course);
+            }
+        }
+
+        return $content;
+    }
+
+    /**
+     * @param Course $course
+     * @return string
+     */
+    private function getCourseContent(Course $course): string
+    {
+        $content = $course->getName();
+
+        foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
+            $content .= $document->getFileName();
+        }
+
+        return $content;
+    }
+
+    private function createZipFile(string $contentHash, array $programs, array $modules, array $courses): string
+    {
+        $fileName = sprintf('%s/data/exports/%s.zip', $this->kernel->getProjectDir(), $contentHash);
+
+        $zip = new ZipArchive();
+        if (!file_exists($fileName) && $zip->open($fileName, ZipArchive::CREATE) === true) {
+            $this->addProgramsToZip($zip, $programs);
+            $this->addModulesToZip($zip, $modules, '');
+            $this->addCoursesToZip($zip, $courses, '');
+            $zip->close();
+        }
+
+        return $fileName;
+    }
+
+    private function addProgramsToZip(ZipArchive $zip, array $programs): void
+    {
+        foreach ($programs as $program) {
+            $programName = $program->getName();
+            $zip->addEmptyDir($programName);
+            $this->addModulesToZip($zip, $program->getModules()->toArray(), $programName);
+        }
+    }
+
+    private function addModulesToZip(ZipArchive $zip, array $modules, string $parentDir): void
+    {
+        foreach ($modules as $module) {
+            $moduleName = $parentDir ? $parentDir . '/' . $module->getName() : $module->getName();
+            $zip->addEmptyDir($moduleName);
+            $this->addModulesToZip($zip, $module->getModules()->toArray(), $moduleName);
+            $this->addCoursesToZip($zip, $module->getCourses()->toArray(), $moduleName);
+        }
+    }
+
+    private function addCoursesToZip(ZipArchive $zip, array $courses, string $parentDir): void
+    {
+        foreach ($courses as $course) {
+            $courseName = $parentDir ? $parentDir . '/' . $course->getName() : $course->getName();
+            $zip->addEmptyDir($courseName);
+            foreach ($this->documentRepository->findByCourseAndHasFile($course) as $document) {
+                $path = $this->storage->resolvePath($document, 'file');
+                $zip->addFile($path, $courseName . '/' . $document->getFileName());
+            }
+        }
+    }
+
+    private function createFileResponse(string $fileName): Response
+    {
+        $response = new Response(file_get_contents($fileName));
+        $response->headers->set('Content-Type', 'application/zip');
+        $response->headers->set('Content-Disposition', 'attachment;filename="document-export-' .
+            (new DateTime())->format('Y-m-d') . '.zip"');
+        $response->headers->set('Content-length', filesize($fileName));
+
+        return $response;
     }
 }
