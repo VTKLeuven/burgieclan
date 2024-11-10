bb4c0e3afc69491fc54e26a3ef7163d709d5b6d6
Robbe Serry <robbeserry@gmail.com>
2024-11-10 22:49:54 +0100

    BUR-174 use test documents in factory

--- backend/src/Factory/DocumentFactory.php
+++ backend/src/Factory/DocumentFactory.php
@@ -73,7 +73,9 @@ final class DocumentFactory extends ModelFactory
             'name' => self::faker()->word(),
             'under_review' => self::faker()->boolean(),
             'creator' => UserFactory::randomOrCreate(),
-            'year' => $this->generateYear(),
+            // Selects a random file from the 'data/documents' directory and assigns its basename to 'file_name'.
+            // Uses the 'glob' function to get all files in the directory and 'randomElement' to pick one randomly.
+            'file_name' => basename(self::faker()->randomElement(glob('data/documents/*'))),                'year' => $this->generateYear(),
         ];
     }
 
