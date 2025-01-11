4b0aeabfc08797b27e7f151e02a06921b5edd783
Robbe Serry <robbeserry@gmail.com>
2025-01-11 19:00:20 +0100

    BUR-216 add page translation

--- backend/src/Entity/Page.php
+++ backend/src/Entity/Page.php
@@ -3,8 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\PageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator as CustomAssert;
 
 #[ORM\Entity(repositoryClass: PageRepository::class)]
 class Page
@@ -13,12 +17,6 @@ class Page
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
-    private string $name;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $content = null;
 
     #[ORM\Column(length: 255, unique: true)]
     private string $urlKey;
@@ -29,44 +27,37 @@ class Page
      */
     private bool $publicAvailable = false;
 
+    public static array $AVAILABLE_LANGUAGES = [
+        'nl' => 'Dutch',
+        'en' => 'English',
+    ];
+
+    public static string $DEFAULT_LANGUAGE = 'nl';
+
+    #[ORM\Column(length: 255)]
+    private ?string $name_nl = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content_nl = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name_en = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content_en = null;
+
     /**
      * @param string $name
      * @param string|null $urlKey
      */
     public function __construct(string $name, ?string $urlKey = null)
     {
-        $this->name = $name;
         $this->urlKey = is_null($urlKey) ? self::createUrlKey($name): self::createUrlKey($urlKey);
     }
-
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(?string $content): static
-    {
-        $this->content = $content;
-
-        return $this;
     }
 
     public function getUrlKey(): ?string
@@ -89,6 +80,64 @@ class Page
     public function setPublicAvailable(bool $publicAvailable): static
     {
         $this->publicAvailable = $publicAvailable;
+
+        return $this;
+    }
+
+    public function getName(string $lang): ?string
+    {
+        return $this->{'name_'.$lang} ?? $this->{'name_'.self::$DEFAULT_LANGUAGE};
+    }
+
+    public function getNameNl(): ?string
+    {
+        return $this->name_nl;
+    }
+
+    public function setNameNl(string $name): static
+    {
+        $this->name_nl = $name;
+
+        return $this;
+    }
+
+    public function getContent(string $lang): ?string
+    {
+        return $this->{'content_'.$lang} ?? $this->{'content_'.self::$DEFAULT_LANGUAGE};
+    }
+
+    public function getContentNl(): ?string
+    {
+        return $this->content_nl;
+    }
+
+    public function setContentNl(string $content): static
+    {
+        $this->content_nl = $content;
+
+        return $this;
+    }
+
+    public function getNameEn(): ?string
+    {
+        return $this->name_en;
+    }
+
+    public function setNameEn(string $name): static
+    {
+        $this->name_en = $name;
+
+        return $this;
+    }
+
+    public function getContentEn(): ?string
+    {
+        return $this->content_en;
+    }
+
+    public function setContentEn(string $content): static
+    {
+        $this->content_en = $content;
 
         return $this;
     }
