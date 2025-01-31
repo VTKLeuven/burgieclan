bca91883d09e1a533f863d11fab2c60c80b9ab1c
Robbe Serry <robbeserry@gmail.com>
2025-01-31 22:11:06 +0100

    BUR-121 add Badge component for review status doc

--- frontend/src/components/account/DocumentList.tsx
+++ frontend/src/components/account/DocumentList.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import useRetrieveDocuments from '@/hooks/useRetrieveDocuments';
 import Link from 'next/link';
 import { LoaderCircle } from 'lucide-react';
-import CollapsibleSection from '../ui/CollapsibleSection';
+import CollapsibleSection from '@/components/ui/CollapsibleSection';
+import Badge from '@/components/ui/Badge';
+import { useTranslation } from 'react-i18next';
 
 const DocumentList: React.FC = () => {
     const { documents, loading } = useRetrieveDocuments();
+    const { t } = useTranslation();
 
     return (
         <CollapsibleSection header={<h3 className="text-xl font-semibold">Uploaded Documents</h3>}>
-            <div className="mt-6 rounded-lg shadow-sm">
-
+            <div className="rounded-lg shadow-sm">
                 {loading ? <LoaderCircle className="animate-spin text-vtk-blue-500" size={48} />
                     : documents.length === 0 ? (
                         <p>No documents uploaded yet.</p>
                     ) : (
-
-                        <ul className="space-y-4">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
                             {documents.map((document) => (
-                                <li key={document.id} className="border p-4 rounded-md shadow-sm">
-                                    <h3 className="text-xl font-semibold">{document.name}</h3>
+                                <div key={document.id} className="border p-4 rounded-md shadow-sm">
+                                    <div className="flex justify-between items-center">
+                                        <h3 className="text-xl font-semibold">{document.name}</h3>
+                                        {document.underReview ? (
+                                            <Badge text={t('document.under_review')} color="yellow" />
+                                        ) : (
+                                            <Badge text={t('document.approved')} color="green" />
+                                        )}
+                                    </div>
                                     <p className="text-gray-700">Course: {document.course!.name}</p>
                                     <p className="text-gray-700">Category: {document.category!.name}</p>
                                     <p className="text-gray-700">Uploaded on: {new Date(document.createDate!).toLocaleString('en-GB', {
@@ -33,9 +41,9 @@ const DocumentList: React.FC = () => {
                                     <Link href={`/document/${document.id}`} className="text-blue-500 hover:underline">
                                         View Document
                                     </Link>
-                                </li>
+                                </div>
                             ))}
-                        </ul>
+                        </div>
                     )}
             </div>
         </CollapsibleSection>
