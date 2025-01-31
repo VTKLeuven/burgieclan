f1a07d10010bd025f1c6726e4eded22b11fe54b6
Robbe Serry <robbeserry@gmail.com>
2025-01-31 17:34:33 +0100

    BUR-121 add CollapsibleSection component

--- frontend/src/components/account/DocumentList.tsx
+++ frontend/src/components/account/DocumentList.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import useRetrieveDocuments from '@/hooks/useRetrieveDocuments';
 import Link from 'next/link';
+import { LoaderCircle } from 'lucide-react';
+import CollapsibleSection from '../ui/CollapsibleSection';
 
 const DocumentList: React.FC = () => {
     const { documents, loading } = useRetrieveDocuments();
 
     return (
-        <div className="bg-white p-6 md:p-10 mx-auto max-w-7xl">
-            <h2 className="text-2xl font-semibold mb-4">Uploaded Documents</h2>
-            {loading ? <p>Loading...</p>
-                : documents.length === 0 ? (
-                    <p>No documents uploaded yet.</p>
-                ) : (
-                    <ul className="space-y-4">
-                        {documents.map((document) => (
-                            <li key={document.id} className="border p-4 rounded-md shadow-sm">
-                                <h3 className="text-xl font-semibold">{document.name}</h3>
-                                <p className="text-gray-700">Course: {document.course!.name}</p>
-                                <p className="text-gray-700">Category: {document.category!.name}</p>
-                                <p className="text-gray-700">Uploaded on: {new Date(document.createDate!).toLocaleString('en-GB', {
-                                    day: '2-digit',
-                                    month: '2-digit',
-                                    year: 'numeric',
-                                    hour: '2-digit',
-                                    minute: '2-digit',
-                                    hour12: false
-                                })}</p>
-                                <Link href={`/document/${document.id}`} className="text-blue-500 hover:underline">
-                                    View Document
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-        </div>
+        <CollapsibleSection header={<h3 className="text-xl font-semibold">Uploaded Documents</h3>}>
+            <div className="mt-6 rounded-lg shadow-sm">
+
+                {loading ? <LoaderCircle className="animate-spin text-vtk-blue-500" size={48} />
+                    : documents.length === 0 ? (
+                        <p>No documents uploaded yet.</p>
+                    ) : (
+
+                        <ul className="space-y-4">
+                            {documents.map((document) => (
+                                <li key={document.id} className="border p-4 rounded-md shadow-sm">
+                                    <h3 className="text-xl font-semibold">{document.name}</h3>
+                                    <p className="text-gray-700">Course: {document.course!.name}</p>
+                                    <p className="text-gray-700">Category: {document.category!.name}</p>
+                                    <p className="text-gray-700">Uploaded on: {new Date(document.createDate!).toLocaleString('en-GB', {
+                                        day: '2-digit',
+                                        month: '2-digit',
+                                        year: 'numeric',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                        hour12: false
+                                    })}</p>
+                                    <Link href={`/document/${document.id}`} className="text-blue-500 hover:underline">
+                                        View Document
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+            </div>
+        </CollapsibleSection>
     );
 };
 
--- frontend/src/components/account/FavoriteList.tsx
+++ frontend/src/components/account/FavoriteList.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronUp, faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as faStarOutline } from '@fortawesome/free-regular-svg-icons';
+import { faStar, faStar as faStarOutline } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import CollapsibleSection from '@/components/ui/CollapsibleSection';
 
 interface FavoriteListProps {
     title: string;
@@ -12,13 +12,7 @@ interface FavoriteListProps {
 }
 
 const FavoriteList: React.FC<FavoriteListProps> = ({ title, items, emptyMessage, updateFavorite }) => {
-    const [isCollapsed, setIsCollapsed] = useState(true);
     const [favorites, setFavorites] = useState(items.map(item => true)); // Assuming all items are initially favorites
-
-    const toggleCollapse = () => {
-        setIsCollapsed(!isCollapsed);
-    };
-
     const toggleFavoriteStatus = (index: number) => {
         const newFavorites = [...favorites];
         newFavorites[index] = !newFavorites[index];
@@ -27,36 +21,32 @@ const FavoriteList: React.FC<FavoriteListProps> = ({ title, items, emptyMessage,
     };
 
     return (
-        <div className="mt-6 border rounded-lg shadow-sm">
-            <button onClick={toggleCollapse} className="w-full text-left focus:outline-none">
-                <div className="flex justify-between items-center px-4 py-1 bg-gray-100 rounded-t-lg">
-                    <h3 className="text-xl font-semibold">
-                        {title} <span className="text-sm">({items.length})</span>
-                    </h3>
-                    <FontAwesomeIcon icon={isCollapsed ? faChevronDown : faChevronUp} className="text-vtk-blue-500" />
+        <CollapsibleSection
+            header={
+                <h3 className="text-xl font-semibold">
+                    {title} <span className="text-sm">({items.length})</span>
+                </h3>
+            }
+        >
+            {items && items.length > 0 ? (
+                <div className='p-4'>
+                    <ul className="space-y-4 list-none">
+                        {items.map((item, index) => (
+                            <li key={index} className="bg-gray-200 px-4 py-2 rounded-md shadow-sm flex justify-between items-center">
+                                <Link href={item.redirectUrl} className="hover:underline flex-1">
+                                    {item.name} {item.code && <span className="text-sm text-gray-500">[{item.code}]</span>}
+                                </Link>
+                                <button onClick={() => toggleFavoriteStatus(index)} className="ml-2 text-red-500">
+                                    <FontAwesomeIcon icon={favorites[index] ? faStar : faStarOutline} className="text-vtk-yellow-500" />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            </button>
-            {!isCollapsed && (
-                <div className="p-4">
-                    {items && items.length > 0 ? (
-                        <ul className="space-y-4 list-none">
-                            {items.map((item, index) => (
-                                <li key={index} className="bg-gray-200 px-4 py-2 rounded-md shadow-sm flex justify-between items-center">
-                                    <Link href={item.redirectUrl} className="hover:underline flex-1">
-                                        {item.name} {item.code && <span className="text-sm text-gray-500">[{item.code}]</span>}
-                                    </Link>
-                                    <button onClick={() => toggleFavoriteStatus(index)} className="ml-2 text-red-500">
-                                        <FontAwesomeIcon icon={favorites[index] ? faStar : faStarOutline} className="text-vtk-yellow-500" />
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p className="text-gray-700">{emptyMessage}</p>
-                    )}
-                </div>
+            ) : (
+                <p>No items available</p>
             )}
-        </div>
+        </CollapsibleSection>
     );
 };
 
