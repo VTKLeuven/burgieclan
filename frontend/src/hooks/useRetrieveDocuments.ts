18297b27c3ae8b5fa5438ea68c41baf987bb7b2a
Robbe Serry <robbeserry@gmail.com>
2025-01-31 23:25:27 +0100

    BUR-121 implement pagination

--- frontend/src/hooks/useRetrieveDocuments.ts
+++ frontend/src/hooks/useRetrieveDocuments.ts
@@ -5,23 +5,26 @@ import { ApiClient } from '@/actions/api';
 import { ApiError } from '@/utils/error/apiError';
 import { convertToDocument } from '@/utils/convertToEntity';
 
-const useRetrieveDocuments = () => {
+const useRetrieveDocuments = (page: number, itemsPerPage: number = 30) => {
     const { user } = useUser();
     const [documents, setDocuments] = useState<Document[]>([]);
+    const [totalItems, setTotalItems] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchDocuments = async (userId?: number) => {
             try {
-                const response = await ApiClient('GET', `/api/documents?creator=${encodeURIComponent(`/api/users/${userId}`)}`);
+                const response = await ApiClient('GET', `/api/documents?creator=${encodeURIComponent(`/api/users/${userId}`)}&page=${page}&itemsPerPage=${itemsPerPage}`);
 
                 if (response?.error) {
                     throw new ApiError(response.error.message, response.error.status);
                 }
 
                 const documents: Document[] = response['hydra:member'].map(convertToDocument);
+                const totalItems: number = response['hydra:totalItems'];
                 setDocuments(documents);
+                setTotalItems(totalItems);
             } catch (err) {
                 setError(err.message);
             } finally {
@@ -32,9 +35,9 @@ const useRetrieveDocuments = () => {
         if (user) {
             fetchDocuments(user.id);
         }
-    }, [user]);
+    }, [user, page, itemsPerPage]);
 
-    return { documents, loading, error };
+    return { documents, totalItems, loading, error };
 };
 
 export default useRetrieveDocuments;
