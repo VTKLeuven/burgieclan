6e6cda738cc1feadca15a33c0ad2d8cf0d2d0f5a
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2025-02-04 22:57:58 +0100

    BUR-70: Moved all progress from other branch

---
+++ frontend/src/app/[locale]/(header-footer)/course/[id]/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import CoursePage from '@/components/coursepage/CoursePage';
+import {Breadcrumb} from '@/types'
+import initTranslations from '@/app/i18n'
+import React from 'react'
+
+const CoursePageWrapper = () => {
+    const { locale, id } = useParams();
+
+    const breadcrumb: Breadcrumb = {
+        breadcrumb: ['Home', 'Courses', `Course ${id}`]
+    };
+
+    if (!id) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div className="flex flex-1 h-full">
+            {/* Main Content */}
+            <div className="flex flex-1">
+                {/* Sidebar */}
+                <aside className="w-64 border-r">
+                    <div className="p-4">
+                        Sidebar content
+                    </div>
+                </aside>
+
+                <CoursePage courseId={Number(id)} breadcrumb={breadcrumb} />
+            </div>
+        </div>
+    );
+};
+
+export default CoursePageWrapper;
---
+++ frontend/src/components/coursepage/CoursePage.tsx
@@ -0,0 +1,172 @@
+'use client'
+import CoursePageSection from "@/components/coursepage/CoursePageSection";
+import { Course, Breadcrumb } from "@/types";
+import Image from 'next/image';
+import DocumentIcon from '/public/images/icons/document_icon.svg';
+import FolderIcon from '/public/images/icons/folder.svg';
+import PiechartIcon from '/public/images/icons/studiepunten.svg';
+import HomeIcon from '/public/images/icons/home.svg';
+import FavoriteStar from '/public/images/icons/favorite_star.svg';
+import { useEffect, useState } from "react";
+import { ApiClient } from "@/actions/api";
+import { ApiError } from "next/dist/server/api-utils";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+
+export default function CoursePage({ courseId, breadcrumb }: { courseId: number, breadcrumb: Breadcrumb }) {
+    const [course, setCourse] = useState<Course | null>(null);
+
+    async function fetchCourse(query: number) {
+        try {
+            return await ApiClient('GET', `/api/courses/${query}`);
+        } catch (err) {
+            throw new ApiError(500, err.message);
+        }
+    }
+
+    useEffect(() => {
+        async function getCourse() {
+            const courseData = await fetchCourse(courseId);
+            setCourse(courseData);
+        }
+        getCourse().catch(console.error);
+    }, [courseId]);
+
+    const ProfessorDiv = ({ unumber, index }: { unumber: string, index: number }) => {
+        const sanitizedUnumber = unumber.replace(/\D/g, '');
+        const [imgSrc, setImgSrc] = useState(`https://www.kuleuven.be/wieiswie/nl/person/0${sanitizedUnumber}/photo`);
+
+        const handleError = () => {
+            setImgSrc('/images/proffen/generic_profile.png');
+        };
+
+        return (
+            <div className="flex items-start h-full">
+                <div className="relative w-16 h-16 overflow-hidden rounded-full border border-yellow-500">
+                    <Image
+                        src={imgSrc}
+                        onError={handleError}
+                        className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-auto"
+                        alt={unumber}
+                        width={64}
+                        height={64}
+                    />
+                </div>
+                <div className="flex flex-col items-start justify-center ml-4 h-full">
+                    <p className="text-lg hover:underline hover:cursor-pointer">L. Beirnaert</p>
+                    {index == 0 && <p className="text-sm text-gray-600">Coördinator</p>}
+                    {!(index == 0) && <p className="text-sm text-gray-600">Professor</p>}
+                </div>
+            </div>
+        );
+    };
+
+    if (!course) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <>
+            <div className="w-full h-full">
+                <div className="min-h-[35%] md:h-[40%] bg-wireframe-lightest-gray relative p-10 pt-5 md:pt-10">
+                    <div>
+                        {/* Breadcrumb */}
+                        <div className="flex flex-col md:flex-row space-x-2">
+                            {breadcrumb.breadcrumb.map((item, index) => (
+                                <div key={item} className="inline-block hover:cursor-pointer">
+                                    {index + 1 < breadcrumb.breadcrumb.length ? (
+                                        <>
+                                            <span className="hover:underline text-wireframe-mid-gray">{item} / </span>
+                                        </>
+                                    ) : (
+                                        <span className="hover:underline">{item}</span>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex items-center space-x-2 mt-3">
+                            <Image src={DocumentIcon} alt="Document Icon" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]" />
+                            <h1 className="md:text-5xl text-4xl mb-4 text-wireframe-primary-blue">{course.name}</h1>
+                        </div>
+
+                        <div className="flex flex-col md:flex-row md:mt-4 mb-4 md:gap-14 gap-2">
+                            <div className="flex items-center space-x-1 gap-2">
+                                <Image src={FolderIcon} alt="Folder Icon" className="w-[24px] h-[24px]" />
+                                <p className="text-lg">{course.code}</p>
+                            </div>
+                            <div className="flex items-center space-x-1 gap-2">
+                                <Image src={PiechartIcon} alt="Piechart Icon" width={24} height={24} />
+                                <p className="text-lg">{course.credits} studiepunten</p>
+                            </div>
+                            <div className="flex items-center space-x-1 gap-2">
+                                <Image src={HomeIcon} alt="Home Icon" width={24} height={24} />
+                                <p className="text-lg">KU Leuven</p>
+                            </div>
+                        </div>
+
+                        <p className="pt-3 md:pt-0 text-lg md:w-[60%] mb-5"> {/*Description top*/}
+                            Lorem ipsum dolor sit amet consectetur.
+                            At orci quis morbi vulputate nibh interdum lectus quam nec.
+                            Ipsum feugiat viverra justo consectetur. Odio commodo aliquet elit.
+                        </p>
+
+
+                        <div className="absolute bottom-0 flex space-x-4 mt-5 mb-5">
+                            <div
+                                className="bg-white rounded-[28px] pl-5 pr-5 pt-2 pb-2 border border-transparent hover:scale-105 hover:border-wireframe-primary-blue hover:cursor-pointer transition-transform duration-300 flex items-center">
+                                <div className="inline-block mr-2">
+                                    <Image src={FavoriteStar} alt="Favorites star" width={17} height={17} />
+                                </div>
+                                <div className="inline-block">
+                                    <p className="text-lg text-wireframe-mid-gray">Favoriet</p>
+                                </div>
+                            </div>
+                            <div
+                                className="bg-white rounded-[28px] pl-5 pr-5 pt-2 pb-2 border border-transparent hover:scale-105 hover:border-wireframe-primary-blue hover:cursor-pointer transition-transform duration-300 flex items-center">
+                                <div className="inline-block mr-2">
+                                    <FontAwesomeIcon icon={faShare} className="text-wireframe-primary-panache" />
+                                </div>
+                                <div className="inline-block">
+                                    <p className="text-lg text-wireframe-mid-gray">Delen</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="flex flex-col md:flex-row md:p-10 pt-7 pl-7 pr-7 md:space-x-2">
+                    <div className="md:w-[60%] mb-4 md:mb-0">
+                        <h2>Over het vak</h2>
+                        <p className="text-lg md:w-[76%]"> {/*Description bottom*/}
+                            Lorem ipsum dolor sit amet consectetur.
+                            At orci quis morbi vulputate nibh interdum lectus quam nec.
+                            Ipsum feugiat viverra justo consectetur.
+                            Odio commodo aliquet elit auctor vulputate in fames condimentum leo.
+                            Venenatis amet ullamcorper pharetra congue arcu at non mi quam.
+                        </p>
+                    </div>
+                    <div className="mb-2 md:mb-0">
+                        <h2>Docenten</h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            {course.professors.map((p, index) => (
+                                <ProfessorDiv key={index} unumber={p} index={index} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="md:p-10 p-7 mb-10">
+                    <h2>Bestanden</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:mt-5 transform scale-90 origin-left">
+                        <CoursePageSection title="Samenvattingen" description="Lorem ipsum dolor sit amet" />
+                        <CoursePageSection title="Werkcollege" description="Lorem ipsum dolor sit amet" />
+                        <CoursePageSection title="Examens" description="Lorem ipsum dolor sit amet" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
