4a2a4d2e26f69a209f7a624496574e7bd78d59f3
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2025-02-05 22:06:19 +0100

    BUR-177: Added nl/en divide in backend for announcements

--- frontend/src/components/announcement/Announcement.tsx
+++ frontend/src/components/announcement/Announcement.tsx
@@ -1,15 +1,18 @@
 import { AlertTriangle, Bell } from 'lucide-react';
 
-export default function Announcement({ priority, title, description }: { priority: number, title: string, description: string }) {
+export default function Announcement({ priority, title, description, datePosted }: { priority: number, title: string, description: string, datePosted: string }) {
     return (
-        <div className="flex">
+        <div className="flex mt-2 mb-2">
             <div className="flex items-center justify-end mr-4">
                 {priority === 2 && <Bell size={32} />}
                 {priority === 1 && <AlertTriangle size={32} />}
             </div>
-            <div className="pl-3 flex flex-col justify-center">
-                <h3 className="text-xl text-wireframe-content">{title}</h3>
+            <div className="pl-5 flex flex-col justify-center">
+                <h3 className="text-xl text-wireframe-content m-0">{title}</h3>
                 <div className="text-wireframe-content" dangerouslySetInnerHTML={{ __html: description }} />
+            </div>
+            <div className="flex items-center justify-end ml-auto mr-6">
+                <span className="text-wireframe-content text-s">{datePosted}</span>
             </div>
         </div>
     );
