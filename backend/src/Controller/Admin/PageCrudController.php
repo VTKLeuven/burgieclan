289a95472720c48f92b1da146a309f770917e7c1
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-02-10 21:38:01 +0100

    BUR-199 merge release 0.7.0 backend

--- backend/src/Controller/Admin/PageCrudController.php
+++ backend/src/Controller/Admin/PageCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Page;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -26,11 +27,24 @@ class PageCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield Textfield::new('name');
-        yield Textfield::new('urlKey');
+        yield Textfield::new('name_nl')
+            ->setRequired(true)
+            ->setLabel('Name');
+        yield Textfield::new('urlKey')
+            ->setHelp('The URL key is used to generate the URL of the page.');
         yield BooleanField::new('publicAvailable')
             ->renderAsSwitch(false);
-        yield TextEditorField::new('content')
+        yield TextEditorField::new('content_nl')
+            ->setLabel('Content')
+            ->setTemplatePath('admin/text_editor.html.twig');
+
+        yield FormField::addPanel('English Content')->setIcon('fa fa-language')
+            ->collapsible()
+            ->renderCollapsed();
+        yield Textfield::new('name_en')
+        ->setLabel('Name');
+        yield TextEditorField::new('content_en')
+            ->setLabel('Content')
             ->setTemplatePath('admin/text_editor.html.twig');
     }
 }
