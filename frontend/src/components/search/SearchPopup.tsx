75354d866bbfc8d599d7cd189cc172287b96f3c0
Robbe Serry <robbeserry@gmail.com>
2025-02-13 12:13:53 +0100

    BUR-75 Enhance search popup backdrop with blur effect

--- frontend/src/components/search/SearchPopup.tsx
+++ frontend/src/components/search/SearchPopup.tsx
@@ -94,7 +94,7 @@ export default function SearchPopup({ open, setOpen }: SearchPopupProps) {
         >
             <DialogBackdrop
                 transition
-                className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+                className="fixed inset-0 backdrop-blur-sm bg-gray-500 bg-opacity-25 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
             />
 
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20">
