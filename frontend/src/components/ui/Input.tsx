8e8f0d3f7997931a3e73a0621e7fd407856cc35a
Robbe Serry <robbeserry@gmail.com>
2025-02-13 12:07:36 +0100

    BUR-75 search dynamic placeholder and remove focus

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -6,19 +6,24 @@ type InputProps = {
     name?: string;
     type?: string;
     placeholder: string;
+    autoComplete?: string;
+    readOnly?: boolean;
+    passive?: boolean;
     onClick?: () => void;
 };
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ id, name, type, placeholder, onClick }, ref) => (
+    ({ id, name, type, placeholder, autoComplete, readOnly, passive, onClick }, ref) => (
         <input
             ref={ref}
             id={id}
             name={name}
             type={type}
             placeholder={placeholder}
+            autoComplete={autoComplete}
+            readOnly={readOnly}
             onClick={onClick}
-            className="block w-full max-h-8 rounded-sm border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className={`block w-full max-h-8 rounded-sm border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ${passive ? 'focus:outline-none focus:ring-1 focus:ring-gray-300' : 'focus:ring-2 focus:ring-inset focus:ring-indigo-600'}`}
         />
     )
 );
