1dc5a9c109a9a8aa9ef248b97499e2e832f5a7ca
Robbe Serry <robbeserry@gmail.com>
2025-02-16 17:35:15 +0100

    BUR-121 update Symfony serializer annotations to attributes

--- backend/src/ApiResource/DocumentCategoryApi.php
+++ backend/src/ApiResource/DocumentCategoryApi.php
@@ -12,7 +12,7 @@ use ApiPlatform\Metadata\GetCollection;
 use App\Entity\DocumentCategory;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityClassDtoStateProvider;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
