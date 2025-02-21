0953390c108eb0552116c877dfa0524096308f6e
Robbe Serry <robbeserry@gmail.com>
2025-02-21 17:46:08 +0100

    BUR-199 memoize options object to optimize rendering

--- frontend/src/components/document/pdf/PDFViewer.tsx
+++ frontend/src/components/document/pdf/PDFViewer.tsx
@@ -6,7 +6,7 @@
  * Based on sample: https://github.com/wojtekmaj/react-pdf/tree/main/sample/next-app/app
  */
 
-import { useState, useEffect } from 'react';
+import { useState, useMemo } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
@@ -33,16 +33,13 @@ export default function PDFViewer({ fileArg, width }: { fileArg: PDFFile, width:
     const [displayedPages, setDisplayedPages] = useState<number>(PAGES_PER_LOAD);
 
     // Options for GET requests to PDF files
-    const options = {
+    // Memoize the options object to prevent unnecessary rerenders
+    const options = useMemo(() => ({
         cMapUrl: '/cmaps/',
         standardFontDataUrl: '/standard_fonts/',
-        httpHeaders: {
-            // Backend requires authentication to access PDFs
-            // TODO: retrieve from context
-            'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3Mzk2NTMzNzAsImV4cCI6MTczOTY1Njk3MCwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJ1c2VybmFtZSI6ImphbmVfYWRtaW4iLCJpZCI6MSwiZnVsbE5hbWUiOiJKYW5lIERvZSJ9.JF7Yh87__M3G6qseum13jIyP4M4lyh4ewPxNk3caSQaX941jKSrGZ5B92xnCSYl-dgMbX30HcJJVfYnafnULyk90gi9fOUVPdKhP5mO8cOx-CmWlYlqHy81CxSoj_UfanTd9ZHQgAfQr5ZDwVxD2uswbqvfVKTfLxGN0evXJezEKYi_GicfzWYyC63jHMrumIerlflz0Tz_xUXu1ykwIAr6g7kBaHZzqO0u8qtYM1E9LoJd1q5j7oGcayy6lvkVw3no-XoIHRyw-jdBhjUXEblKpEyEDW318pSqCU8AuIwE6awWvCSbGoLGO84UQ998SXQzYiKr9IEmDTNc2GUgehQ`,
-        },
         withCredentials: true,
-    };
+    }), []); // Empty dependency array since these values never change
+
 
     function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
         setNumPages(nextNumPages);
