6d0cf555f5e1e9c188a0782f1f9d1c7b00a8a33f
Robbe Serry <robbeserry@gmail.com>
2025-03-03 20:45:51 +0100

    BUR-162 fix foreign key constraints in migration

--- backend/migrations/Version20250302233402.php
+++ backend/migrations/Version20250302233402.php
@@ -19,12 +19,17 @@ final class Version20250302233402 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE course_identical_courses (course_source INT NOT NULL, course_target INT NOT NULL, INDEX IDX_9A81ADB8F1B2BE3E (course_source), INDEX IDX_9A81ADB8E857EEB1 (course_target), PRIMARY KEY(course_source, course_target)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE course_identical_courses ADD CONSTRAINT FK_9A81ADB8F1B2BE3E FOREIGN KEY (course_source) REFERENCES course (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE course_identical_courses ADD CONSTRAINT FK_9A81ADB8E857EEB1 FOREIGN KEY (course_target) REFERENCES course (id) ON DELETE CASCADE');}
+        $this->addSql('ALTER TABLE course_identical_courses ADD CONSTRAINT FK_9A81ADB8E857EEB1 FOREIGN KEY (course_target) REFERENCES course (id) ON DELETE CASCADE');
+    }
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE course_identical_courses DROP FOREIGN KEY FK_9A81ADB8F1B2BE3E');
+        $this->addSql('ALTER TABLE course_identical_courses DROP FOREIGN KEY FK_9A81ADB8E857EEB1');
         $this->addSql('DROP TABLE course_identical_courses');
     }
 }
