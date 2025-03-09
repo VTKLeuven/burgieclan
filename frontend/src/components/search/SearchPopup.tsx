345ff5657688ecd56c4fc2488932208026d451ae
Robbe Serry <robbeserry@gmail.com>
2025-03-09 15:38:49 +0100

    BUR-265 add single div inside combobox to avoid weird bug

--- frontend/src/components/search/SearchPopup.tsx
+++ frontend/src/components/search/SearchPopup.tsx
@@ -111,118 +111,120 @@ export default function SearchPopup({ open, setOpen }: SearchPopupProps) {
                             }
                         }}
                     >
-                        <div className="relative">
-                            <SearchIcon
-                                className="pointer-events-none absolute left-4 top-3.5 h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                            <ComboboxInput
-                                autoFocus
-                                className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                                placeholder={t('search.placeholder')}
-                                onChange={(event) => setQuery(event.target.value)}
-                                onBlur={() => setQuery('')}
-                            />
+                        <div>
+                            <div className="relative">
+                                <SearchIcon
+                                    className="pointer-events-none absolute left-4 top-3.5 h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                />
+                                <ComboboxInput
+                                    autoFocus
+                                    className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+                                    placeholder={t('search.placeholder')}
+                                    onChange={(event) => setQuery(event.target.value)}
+                                    onBlur={() => setQuery('')}
+                                />
+                            </div>
+
+                            {loading && (
+                                <div className="flex justify-center py-12">
+                                    <svg className="animate-spin h-8 w-8 text-gray-900" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 24 24">
+                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                            strokeWidth="4"></circle>
+                                        <path className="opacity-75" fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+                                    </svg>
+                                </div>
+                            )}
+
+                            {error && (
+                                <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
+                                    <Frown className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
+                                    <p className="mt-4 font-semibold text-gray-900">{t('unexpected_error')}</p>
+                                    <p className="mt-2 text-gray-500">{error.message}</p>
+                                </div>
+                            )}
+
+                            {!error && query.length <= 2 && (
+                                <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
+                                    <Globe className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
+                                    <p className="mt-4 font-semibold text-gray-900">{t('search.info')}</p>
+                                    <p className="mt-2 text-gray-500">{t('search.info_sub')}</p>
+                                </div>
+                            )}
+
+                            {!error && query.length > 2 && Object.values(items).some(value => Array.isArray(value) && value.length > 0) && (
+                                <div>
+                                    <ComboboxOptions
+                                        static
+                                        as="ul"
+                                        className="max-h-[calc(100vh-15rem)] scroll-pb-2 scroll-pt-11 space-y-2 overflow-y-auto pb-2 pl-0"
+                                    >
+                                        {items.courses.length > 0 &&
+                                            <FoldableSection key={'courses'} title={t('courses')} defaultOpen={true}>
+                                                <li className="list-none">
+                                                    <ul
+                                                        className="mt-2 text-sm text-gray-800 pl-0"
+                                                    >
+                                                        {items.courses.map((course) => (
+                                                            <CourseSearchResult key={course.id} course={course} />
+                                                        ))}
+                                                    </ul>
+                                                </li>
+                                            </FoldableSection>
+                                        }
+                                        {items.modules.length > 0 &&
+                                            <FoldableSection key={'modules'} title={t('modules')} defaultOpen={true}>
+                                                <li className="list-none">
+                                                    <ul
+                                                        className="mt-2 text-sm text-gray-800 pl-0"
+                                                    >
+                                                        {items.modules.map((module) => (
+                                                            <ModuleSearchResult key={module.id} module={module} />
+                                                        ))}
+                                                    </ul>
+                                                </li>
+                                            </FoldableSection>
+                                        }
+                                        {items.programs.length > 0 &&
+                                            <FoldableSection key={'programs'} title={t('programs')} defaultOpen={true}>
+                                                <li className="list-none">
+                                                    <ul
+                                                        className="mt-2 text-sm text-gray-800 pl-0"
+                                                    >
+                                                        {items.programs.map((program) => (
+                                                            <ProgramSearchResult key={program.id} program={program} />
+                                                        ))}
+                                                    </ul>
+                                                </li>
+                                            </FoldableSection>
+                                        }
+                                        {items.documents.length > 0 &&
+                                            <FoldableSection key={'documents'} title={t('documents')} defaultOpen={true}>
+                                                <li className="list-none">
+                                                    <ul
+                                                        className="mt-2 text-sm text-gray-800 pl-0"
+                                                    >
+                                                        {items.documents.map((document) => (
+                                                            <DocumentSearchResult key={document.id} document={document} />
+                                                        ))}
+                                                    </ul>
+                                                </li>
+                                            </FoldableSection>
+                                        }
+                                    </ComboboxOptions>
+                                </div>
+                            )}
+
+                            {!error && !loading && query.length > 2 && Object.values(items).every(value => Array.isArray(value) && value.length === 0) && (
+                                <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
+                                    <Frown className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
+                                    <p className="mt-4 font-semibold text-gray-900">{t('search.no_results')}</p>
+                                    <p className="mt-2 text-gray-500">{t('search.no_results_sub')}</p>
+                                </div>
+                            )}
                         </div>
-
-                        {loading && (
-                            <div className="flex justify-center py-12">
-                                <svg className="animate-spin h-8 w-8 text-gray-900" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                        strokeWidth="4"></circle>
-                                    <path className="opacity-75" fill="currentColor"
-                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
-                                </svg>
-                            </div>
-                        )}
-
-                        {error && (
-                            <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
-                                <Frown className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
-                                <p className="mt-4 font-semibold text-gray-900">{t('unexpected_error')}</p>
-                                <p className="mt-2 text-gray-500">{error.message}</p>
-                            </div>
-                        )}
-
-                        {!error && query.length <= 2 && (
-                            <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
-                                <Globe className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
-                                <p className="mt-4 font-semibold text-gray-900">{t('search.info')}</p>
-                                <p className="mt-2 text-gray-500">{t('search.info_sub')}</p>
-                            </div>
-                        )}
-
-                        {!error && query.length > 2 && Object.values(items).some(value => Array.isArray(value) && value.length > 0) && (
-                            <div>
-                                <ComboboxOptions
-                                    static
-                                    as="ul"
-                                    className="max-h-[calc(100vh-15rem)] scroll-pb-2 scroll-pt-11 space-y-2 overflow-y-auto pb-2 pl-0"
-                                >
-                                    {items.courses.length > 0 &&
-                                        <FoldableSection key={'courses'} title={t('courses')} defaultOpen={true}>
-                                            <li className="list-none">
-                                                <ul
-                                                    className="mt-2 text-sm text-gray-800 pl-0"
-                                                >
-                                                    {items.courses.map((course) => (
-                                                        <CourseSearchResult key={course.id} course={course} />
-                                                    ))}
-                                                </ul>
-                                            </li>
-                                        </FoldableSection>
-                                    }
-                                    {items.modules.length > 0 &&
-                                        <FoldableSection key={'modules'} title={t('modules')} defaultOpen={true}>
-                                            <li className="list-none">
-                                                <ul
-                                                    className="mt-2 text-sm text-gray-800 pl-0"
-                                                >
-                                                    {items.modules.map((module) => (
-                                                        <ModuleSearchResult key={module.id} module={module} />
-                                                    ))}
-                                                </ul>
-                                            </li>
-                                        </FoldableSection>
-                                    }
-                                    {items.programs.length > 0 &&
-                                        <FoldableSection key={'programs'} title={t('programs')} defaultOpen={true}>
-                                            <li className="list-none">
-                                                <ul
-                                                    className="mt-2 text-sm text-gray-800 pl-0"
-                                                >
-                                                    {items.programs.map((program) => (
-                                                        <ProgramSearchResult key={program.id} program={program} />
-                                                    ))}
-                                                </ul>
-                                            </li>
-                                        </FoldableSection>
-                                    }
-                                    {items.documents.length > 0 &&
-                                        <FoldableSection key={'documents'} title={t('documents')} defaultOpen={true}>
-                                            <li className="list-none">
-                                                <ul
-                                                    className="mt-2 text-sm text-gray-800 pl-0"
-                                                >
-                                                    {items.documents.map((document) => (
-                                                        <DocumentSearchResult key={document.id} document={document} />
-                                                    ))}
-                                                </ul>
-                                            </li>
-                                        </FoldableSection>
-                                    }
-                                </ComboboxOptions>
-                            </div>
-                        )}
-
-                        {!error && !loading && query.length > 2 && Object.values(items).every(value => Array.isArray(value) && value.length === 0) && (
-                            <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
-                                <Frown className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
-                                <p className="mt-4 font-semibold text-gray-900">{t('search.no_results')}</p>
-                                <p className="mt-2 text-gray-500">{t('search.no_results_sub')}</p>
-                            </div>
-                        )}
                     </Combobox>
                 </DialogPanel>
             </div>
