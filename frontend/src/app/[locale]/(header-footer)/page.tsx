a5d200e436dae2cfb0a33c0614b4e078fb358ca7
Robbe Serry <robbeserry@gmail.com>
2025-03-09 15:57:04 +0100

    BUR-270 only load sidebar when logged in

--- frontend/src/app/[locale]/(header-footer)/page.tsx
+++ frontend/src/app/[locale]/(header-footer)/page.tsx
@@ -1,7 +1,6 @@
 import initTranslations from "@/app/i18n";
 import React from 'react';
 import HomePage from '@/components/homepage/HomePage';
-import Sidebar from '@/components/layout/Sidebar';
 
 export default async function Homepage({ params: { locale } }: { params: { locale: string } }) {
     const { t } = await initTranslations(locale);
