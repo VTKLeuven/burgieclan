f8cab6aa6382cdbe39a0a616cacbb486adc2bc53
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-03-10 23:38:48 +0100

    BUR-184 add translations

--- frontend/src/components/header/HeaderProfileButton.tsx
+++ frontend/src/components/header/HeaderProfileButton.tsx
@@ -12,12 +12,16 @@ import {
 import { CircleUserRound } from 'lucide-react';
 import { logOut } from "@/actions/oauth";
 import { useRouter } from 'next/navigation'
-import {useUser} from "@/components/UserContext";
+import { useUser } from "@/components/UserContext";
+import { useTranslation } from 'react-i18next';
+import Link from "next/link";
 
 /**
  * Shows login button if unauthenticated, otherwise dropdown with profile options
  */
 export default function HeaderProfileButton() {
+    const { t } = useTranslation();
+
     const router = useRouter()
 
     const { user } = useUser();
@@ -39,15 +43,15 @@ export default function HeaderProfileButton() {
                         <DropdownMenuLabel className="pt-1.5 pb-1">{user?.fullName}</DropdownMenuLabel>
                         <DropdownMenuLabel className="text-gray-600 pt-0 pb-1.5 text-xs">{user?.username}</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem><a className="font-normal" href="profile">Profile</a></DropdownMenuItem>
-                        <DropdownMenuItem><a className="font-normal" href="https://vtk.be/account/">My VTK</a></DropdownMenuItem>
-                        <DropdownMenuItem><button className="font-normal" onClick={async () => onClickLogout()}>Log out</button></DropdownMenuItem>
+                        <DropdownMenuItem><Link className="font-normal text-sm" href="account">{t('account.account')}</Link></DropdownMenuItem>
+                        <DropdownMenuItem><Link className="font-normal text-sm" href="https://vtk.be/account/">{t('header.my_vtk')}</Link></DropdownMenuItem>
+                        <DropdownMenuItem><button className="text-sm leading-6 text-gray-900" onClick={async () => onClickLogout()}>{t('logout')}</button></DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
                 :
-                <a href="login" className="primary-button">
-                    Login
-                </a>
+                <Link href="login" className="primary-button">
+                    {t('login')}
+                </Link>
             }
         </>
     )
