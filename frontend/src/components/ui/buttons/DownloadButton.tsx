3661fe0746a27a71d0fc9f92f99e591d751e8669
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-03-10 22:28:58 +0100

    BUR-199 fix download button loading icon

--- frontend/src/components/ui/buttons/DownloadButton.tsx
+++ frontend/src/components/ui/buttons/DownloadButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Download } from "lucide-react";
+import {Download, Loader} from "lucide-react";
 import { useState } from "react";
 
 interface DownloadButtonProps {
@@ -11,11 +11,11 @@ interface DownloadButtonProps {
 }
 
 export default function DownloadButton({
-    contentUrl,
-    fileName,
-    fileSize,
-    disabled = false,
-}: DownloadButtonProps) {
+                                           contentUrl,
+                                           fileName,
+                                           fileSize,
+                                           disabled = false,
+                                       }: DownloadButtonProps) {
     const [isDownloading, setIsDownloading] = useState(false);
     const [isHovered, setIsHovered] = useState(false); // Used to show file size on hover
 
@@ -25,9 +25,7 @@ export default function DownloadButton({
 
         try {
             const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + contentUrl, {
-                headers: {
-                    'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3Mzk2NTMzNzAsImV4cCI6MTczOTY1Njk3MCwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJ1c2VybmFtZSI6ImphbmVfYWRtaW4iLCJpZCI6MSwiZnVsbE5hbWUiOiJKYW5lIERvZSJ9.JF7Yh87__M3G6qseum13jIyP4M4lyh4ewPxNk3caSQaX941jKSrGZ5B92xnCSYl-dgMbX30HcJJVfYnafnULyk90gi9fOUVPdKhP5mO8cOx-CmWlYlqHy81CxSoj_UfanTd9ZHQgAfQr5ZDwVxD2uswbqvfVKTfLxGN0evXJezEKYi_GicfzWYyC63jHMrumIerlflz0Tz_xUXu1ykwIAr6g7kBaHZzqO0u8qtYM1E9LoJd1q5j7oGcayy6lvkVw3no-XoIHRyw-jdBhjUXEblKpEyEDW318pSqCU8AuIwE6awWvCSbGoLGO84UQ998SXQzYiKr9IEmDTNc2GUgehQ`,
-                }
+                credentials: 'include'
             });
 
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
@@ -63,7 +61,7 @@ export default function DownloadButton({
                 transition-all duration-300 ease-in-out
                 ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}
                 border-gray-500 relative
-                ${isHovered ? 'w-24' : 'w-12'}
+                ${isDownloading ? 'w-18' : `${isHovered ? 'w-24' : 'w-12'}`}
             `}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
@@ -72,26 +70,29 @@ export default function DownloadButton({
         >
             <Download
                 size={20}
-                strokeWidth={isHovered ? '2.5' : '1.5'}
+                strokeWidth={isHovered && (!isDownloading) ? '2.5' : '1.5'}
                 className={`
-                    transition-all duration-300
-                    ${isDownloading ? 'text-green-500 animate-pulse' : 'text-gray-500'}
+                    transition-all duration-300 text-gray-500
                     ${isHovered ? '-translate-x-1' : ''}
+                    ${isDownloading ? 'opacity-0 duration-0' : 'opacity-100 duration-300'}
                 `}
             />
 
             <span
                 className={`
-                    absolute 
+                    absolute
                     right-3
                     text-sm
                     font-semibold
                     text-gray-500
                     transition-opacity
-                    ${isHovered ? 'opacity-100 duration-300' : 'opacity-0 duration-0'}
+                    ${isDownloading ? 'opacity-100 duration-300' : `${isHovered ? 'opacity-100 duration-300' : 'opacity-0 duration-0'}`}
                 `}
             >
-                {fileSize}
+                {isDownloading
+                    ? <Loader size={20} className="animate-spin"/>
+                    : fileSize
+                }
             </span>
         </button>
     );
