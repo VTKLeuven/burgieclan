439d44b0eccc52f1adab81509f697c2ccdfa3a94
Robbe Serry <robbeserry@gmail.com>
2025-03-16 17:19:51 +0100

    BUR-271 update zenstruck/foundry and refactor Factories

--- backend/src/Factory/PageFactory.php
+++ backend/src/Factory/PageFactory.php
@@ -13,46 +13,47 @@ namespace App\Factory;
 
 use App\Entity\Page;
 use App\Repository\PageRepository;
-use Zenstruck\Foundry\ModelFactory;
-use Zenstruck\Foundry\Proxy;
-use Zenstruck\Foundry\RepositoryProxy;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
 
 /**
- * @extends ModelFactory<Page>
+ * @extends PersistentProxyObjectFactory<Page>
  *
- * @method        Page|Proxy                     create(array|callable $attributes = [])
- * @method static Page|Proxy                     createOne(array $attributes = [])
- * @method static Page|Proxy                     find(object|array|mixed $criteria)
- * @method static Page|Proxy                     findOrCreate(array $attributes)
- * @method static Page|Proxy                     first(string $sortedField = 'id')
- * @method static Page|Proxy                     last(string $sortedField = 'id')
- * @method static Page|Proxy                     random(array $attributes = [])
- * @method static Page|Proxy                     randomOrCreate(array $attributes = [])
- * @method static PageRepository|RepositoryProxy repository()
- * @method static Page[]|Proxy[]                 all()
- * @method static Page[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
- * @method static Page[]|Proxy[]                 createSequence(iterable|callable $sequence)
- * @method static Page[]|Proxy[]                 findBy(array $attributes)
- * @method static Page[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
- * @method static Page[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @method        Page|Proxy                              create(array|callable $attributes = [])
+ * @method static Page|Proxy                              createOne(array $attributes = [])
+ * @method static Page|Proxy                              find(object|array|mixed $criteria)
+ * @method static Page|Proxy                              findOrCreate(array $attributes)
+ * @method static Page|Proxy                              first(string $sortedField = 'id')
+ * @method static Page|Proxy                              last(string $sortedField = 'id')
+ * @method static Page|Proxy                              random(array $attributes = [])
+ * @method static Page|Proxy                              randomOrCreate(array $attributes = [])
+ * @method static PageRepository|ProxyRepositoryDecorator repository()
+ * @method static Page[]|Proxy[]                          all()
+ * @method static Page[]|Proxy[]                          createMany(int $number, array|callable $attributes = [])
+ * @method static Page[]|Proxy[]                          createSequence(iterable|callable $sequence)
+ * @method static Page[]|Proxy[]                          findBy(array $attributes)
+ * @method static Page[]|Proxy[]                          randomRange(int $min, int $max, array $attributes = [])
+ * @method static Page[]|Proxy[]                          randomSet(int $number, array $attributes = [])
  *
- * @phpstan-method        Proxy<Page> create(array|callable $attributes = [])
- * @phpstan-method static Proxy<Page> createOne(array $attributes = [])
- * @phpstan-method static Proxy<Page> find(object|array|mixed $criteria)
- * @phpstan-method static Proxy<Page> findOrCreate(array $attributes)
- * @phpstan-method static Proxy<Page> first(string $sortedField = 'id')
- * @phpstan-method static Proxy<Page> last(string $sortedField = 'id')
- * @phpstan-method static Proxy<Page> random(array $attributes = [])
- * @phpstan-method static Proxy<Page> randomOrCreate(array $attributes = [])
- * @phpstan-method static RepositoryProxy<Page> repository()
- * @phpstan-method static list<Proxy<Page>> all()
- * @phpstan-method static list<Proxy<Page>> createMany(int $number, array|callable $attributes = [])
- * @phpstan-method static list<Proxy<Page>> createSequence(iterable|callable $sequence)
- * @phpstan-method static list<Proxy<Page>> findBy(array $attributes)
- * @phpstan-method static list<Proxy<Page>> randomRange(int $min, int $max, array $attributes = [])
- * @phpstan-method static list<Proxy<Page>> randomSet(int $number, array $attributes = [])
+ * @phpstan-method        Page&Proxy<Page> create(array|callable $attributes = [])
+ * @phpstan-method static Page&Proxy<Page> createOne(array $attributes = [])
+ * @phpstan-method static Page&Proxy<Page> find(object|array|mixed $criteria)
+ * @phpstan-method static Page&Proxy<Page> findOrCreate(array $attributes)
+ * @phpstan-method static Page&Proxy<Page> first(string $sortedField = 'id')
+ * @phpstan-method static Page&Proxy<Page> last(string $sortedField = 'id')
+ * @phpstan-method static Page&Proxy<Page> random(array $attributes = [])
+ * @phpstan-method static Page&Proxy<Page> randomOrCreate(array $attributes = [])
+ * @phpstan-method static ProxyRepositoryDecorator<Page, EntityRepository> repository()
+ * @phpstan-method static list<Page&Proxy<Page>> all()
+ * @phpstan-method static list<Page&Proxy<Page>> createMany(int $number, array|callable $attributes = [])
+ * @phpstan-method static list<Page&Proxy<Page>> createSequence(iterable|callable $sequence)
+ * @phpstan-method static list<Page&Proxy<Page>> findBy(array $attributes)
+ * @phpstan-method static list<Page&Proxy<Page>> randomRange(int $min, int $max, array $attributes = [])
+ * @phpstan-method static list<Page&Proxy<Page>> randomSet(int $number, array $attributes = [])
  */
-final class PageFactory extends ModelFactory
+final class PageFactory extends PersistentProxyObjectFactory
 {
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
@@ -62,10 +63,15 @@ final class PageFactory extends ModelFactory
         parent::__construct();
     }
 
+    public static function class(): string
+    {
+        return Page::class;
+    }
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
      */
-    protected function getDefaults(): array
+    protected function defaults(): array|callable
     {
         $name = self::faker()->text(20);
         return [
@@ -81,15 +87,10 @@ final class PageFactory extends ModelFactory
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
      */
-    protected function initialize(): self
+    protected function initialize(): static
     {
         return $this
             // ->afterInstantiate(function(Page $page): void {})
         ;
-    }
-
-    protected static function getClass(): string
-    {
-        return Page::class;
     }
 }
