6ed73747d4e6db994f6f5515371ed2a8a5507403
pedrodevog <105945602+pedrodevog@users.noreply.github.com>
2025-03-18 12:34:25 +0100

    Update backend/src/ApiResource/UserDocumentViewApi.php
    Co-authored-by: Robbe Serry <53912249+rserry@users.noreply.github.com>

--- backend/src/ApiResource/UserDocumentViewApi.php
+++ backend/src/ApiResource/UserDocumentViewApi.php
@@ -9,7 +9,7 @@ use ApiPlatform\Metadata\GetCollection;
 use App\Entity\UserDocumentView;
 use App\State\UserDocumentViewProvider;
 use DateTimeInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
