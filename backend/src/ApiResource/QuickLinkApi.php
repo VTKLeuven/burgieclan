4bded1998dece8ecaff8a6cc64e7e6e4ad5f092c
Robbe Serry <robbeserry@gmail.com>
2025-03-20 21:55:33 +0100

    BUR-226 Add translations to QuickLink

--- backend/src/ApiResource/QuickLinkApi.php
+++ backend/src/ApiResource/QuickLinkApi.php
@@ -2,9 +2,7 @@
 
 namespace App\ApiResource;
 
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\Options;
-use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
@@ -16,8 +14,40 @@ use App\State\EntityClassDtoStateProvider;
 #[ApiResource(
     shortName: 'Quick Link',
     operations: [
-        new Get(),
-        new GetCollection(),
+        new Get(
+            openapiContext: [
+                'parameters' => [
+                    [
+                        'name' => 'lang',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                            'enum' => ['nl', 'en'],
+                            'default' => 'nl'
+                        ],
+                        'description' => 'The language in which to retrieve the page content'
+                    ]
+                ]
+            ]
+        ),
+        new GetCollection(
+            openapiContext: [
+                'parameters' => [
+                    [
+                        'name' => 'lang',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                            'enum' => ['nl', 'en'],
+                            'default' => 'nl'
+                        ],
+                        'description' => 'The language in which to retrieve the page content'
+                    ]
+                ]
+            ]
+        ),
     ],
     provider: EntityClassDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
@@ -28,9 +58,7 @@ class QuickLinkApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
-    #[ApiFilter(SearchFilter::class, strategy: 'ipartial')]
     public ?string $name = null;
 
-    #[ApiFilter(SearchFilter::class, strategy: 'ipartial')]
     public ?string $linkTo = null;
 }
--- backend/src/Entity/QuickLink.php
+++ backend/src/Entity/QuickLink.php
@@ -13,8 +13,13 @@ class QuickLink
     #[ORM\Column]
     private ?int $id = null;
 
+    public static string $DEFAULT_LANGUAGE = 'nl';
+
     #[ORM\Column(length: 255, nullable: true)]
-    private ?string $name = null;
+    private ?string $name_nl = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name_en = null;
 
     #[ORM\Column(length: 255)]
     private ?string $linkTo = null;
@@ -24,14 +29,31 @@ class QuickLink
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(string $lang): ?string
     {
-        return $this->name;
+        return $this->{'name_'.$lang} ?? $this->{'name_'.self::$DEFAULT_LANGUAGE};
     }
 
-    public function setName(?string $name): static
+    public function getNameNl(): ?string
     {
-        $this->name = $name;
+        return $this->name_nl;
+    }
+
+    public function setNameNl(?string $name): static
+    {
+        $this->name_nl = $name;
+
+        return $this;
+    }
+
+    public function getNameEn(): ?string
+    {
+        return $this->name_en;
+    }
+
+    public function setNameEn(?string $name): static
+    {
+        $this->name_en = $name;
 
         return $this;
     }
