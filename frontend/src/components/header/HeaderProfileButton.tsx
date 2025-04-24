5f562a3ecc0345282741247c1f9919c8b2a1b423
Robbe Serry <robbeserry@gmail.com>
2025-04-24 21:10:46 +0200

    BUR-184 improve styling profile button and add translations

--- frontend/src/components/header/HeaderProfileButton.tsx
+++ frontend/src/components/header/HeaderProfileButton.tsx
@@ -9,7 +9,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { CircleUserRound } from 'lucide-react';
+import { CircleUserRound, ExternalLink } from 'lucide-react';
 import { logOut } from "@/actions/oauth";
 import { useRouter } from 'next/navigation'
 import { useUser } from "@/components/UserContext";
@@ -25,26 +25,38 @@ export default function HeaderProfileButton() {
     const router = useRouter()
 
     const { user } = useUser();
-    const isAuthenticated = user !== null;
 
     const onClickLogout = async () => {
         await logOut();
         await router.push("/login");
     }
 
-    return(
+    return (
         <>
             <DropdownMenu>
                 <DropdownMenuTrigger>
-                    <CircleUserRound className="" size={28} strokeWidth={1.5}/>
+                    <CircleUserRound className="" size={28} strokeWidth={1.5} />
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel className="pt-1.5 pb-1">{user?.fullName}</DropdownMenuLabel>
                     <DropdownMenuLabel className="text-gray-600 pt-0 pb-1.5 text-xs">{user?.username}</DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem><Link className="font-normal text-sm" href="/account">{t('account.account')}</Link></DropdownMenuItem>
-                    <DropdownMenuItem><Link className="font-normal text-sm" href="https://vtk.be/account/">{t('header.my_vtk')}</Link></DropdownMenuItem>
-                    <DropdownMenuItem><button className="text-sm leading-6 text-gray-900" onClick={async () => onClickLogout()}>{t('logout')}</button></DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link className="font-normal text-sm flex items-center gap-1" href="https://vtk.be/account/">
+                            {t('header.my_vtk')}
+                            <ExternalLink size={14} className="ml-1" />
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <button
+                            className="text-sm w-full text-left text-red-600 hover:text-red-800 font-medium"
+                            onClick={async () => onClickLogout()}
+                        >
+                            {t('logout')}
+                        </button>
+                    </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
