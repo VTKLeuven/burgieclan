2093b6cefd74b61734cf98407a63a17585a09d13
Robbe Serry <robbeserry@gmail.com>
2025-04-24 20:41:53 +0200

    BUR-271 refactor QuickLinkFactory for new zenstruck version

--- backend/src/Factory/QuickLinkFactory.php
+++ backend/src/Factory/QuickLinkFactory.php
@@ -12,47 +12,12 @@
 namespace App\Factory;
 
 use App\Entity\QuickLink;
-use App\Repository\QuickLinkRepository;
-use Zenstruck\Foundry\ModelFactory;
-use Zenstruck\Foundry\Proxy;
-use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
 /**
- * @extends ModelFactory<QuickLink>
- *
- * @method        QuickLink|Proxy                     create(array|callable $attributes = [])
- * @method static QuickLink|Proxy                     createOne(array $attributes = [])
- * @method static QuickLink|Proxy                     find(object|array|mixed $criteria)
- * @method static QuickLink|Proxy                     findOrCreate(array $attributes)
- * @method static QuickLink|Proxy                     first(string $sortedField = 'id')
- * @method static QuickLink|Proxy                     last(string $sortedField = 'id')
- * @method static QuickLink|Proxy                     random(array $attributes = [])
- * @method static QuickLink|Proxy                     randomOrCreate(array $attributes = [])
- * @method static QuickLinkRepository|RepositoryProxy repository()
- * @method static QuickLink[]|Proxy[]                 all()
- * @method static QuickLink[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
- * @method static QuickLink[]|Proxy[]                 createSequence(iterable|callable $sequence)
- * @method static QuickLink[]|Proxy[]                 findBy(array $attributes)
- * @method static QuickLink[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
- * @method static QuickLink[]|Proxy[]                 randomSet(int $number, array $attributes = [])
- *
- * @phpstan-method        Proxy<QuickLink> create(array|callable $attributes = [])
- * @phpstan-method static Proxy<QuickLink> createOne(array $attributes = [])
- * @phpstan-method static Proxy<QuickLink> find(object|array|mixed $criteria)
- * @phpstan-method static Proxy<QuickLink> findOrCreate(array $attributes)
- * @phpstan-method static Proxy<QuickLink> first(string $sortedField = 'id')
- * @phpstan-method static Proxy<QuickLink> last(string $sortedField = 'id')
- * @phpstan-method static Proxy<QuickLink> random(array $attributes = [])
- * @phpstan-method static Proxy<QuickLink> randomOrCreate(array $attributes = [])
- * @phpstan-method static RepositoryProxy<QuickLink> repository()
- * @phpstan-method static list<Proxy<QuickLink>> all()
- * @phpstan-method static list<Proxy<QuickLink>> createMany(int $number, array|callable $attributes = [])
- * @phpstan-method static list<Proxy<QuickLink>> createSequence(iterable|callable $sequence)
- * @phpstan-method static list<Proxy<QuickLink>> findBy(array $attributes)
- * @phpstan-method static list<Proxy<QuickLink>> randomRange(int $min, int $max, array $attributes = [])
- * @phpstan-method static list<Proxy<QuickLink>> randomSet(int $number, array $attributes = [])
+ * @extends PersistentProxyObjectFactory<QuickLink>
  */
-final class QuickLinkFactory extends ModelFactory
+final class QuickLinkFactory extends PersistentProxyObjectFactory
 {
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
@@ -62,10 +27,15 @@ final class QuickLinkFactory extends ModelFactory
         parent::__construct();
     }
 
+    public static function class(): string
+    {
+        return QuickLink::class;
+    }
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
      */
-    protected function getDefaults(): array
+    protected function defaults(): array|callable
     {
         $name = self::faker()->text(20);
         return [
@@ -78,15 +48,10 @@ final class QuickLinkFactory extends ModelFactory
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
      */
-    protected function initialize(): self
+    protected function initialize(): static
     {
         return $this
             // ->afterInstantiate(function(QuickLink $quickLink): void {})
         ;
-    }
-
-    protected static function getClass(): string
-    {
-        return QuickLink::class;
     }
 }
