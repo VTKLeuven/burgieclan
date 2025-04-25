edb119922677e5ddb8796ecbe821f57e0c2d7662
Robbe Serry <robbeserry@gmail.com>
2025-04-25 09:56:24 +0200

    BUR-193 use useApi for data fetching + error handling

--- frontend/src/components/courses/CurriculumNavigator.tsx
+++ frontend/src/components/courses/CurriculumNavigator.tsx
@@ -1,47 +1,35 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import { ApiClient } from "@/actions/api";
 import Loading from '@/app/[locale]/loading';
 import { convertToCourse } from "@/utils/convertToEntity";
 import type { Course } from '@/types/entities';
-import { ApiError } from '@/utils/error/apiError';
 import { CourseRow } from '@/components/courses/CourseRow';
 import { CourseTableHeader } from '@/components/courses/CourseTableHeader';
-import ErrorPage from '@/components/error/ErrorPage';
+import { useApi } from '@/hooks/useApi';
+import { useTranslation } from 'react-i18next';
 
 export default function CurriculumNavigator() {
     const [courses, setCourses] = useState<Course[]>([]);
-    const [error, setError] = useState<ApiError | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
+    const { request, loading, error } = useApi();
+    const { t } = useTranslation();
 
     useEffect(() => {
         const fetchData = async () => {
-            try {
-                const result = await ApiClient('GET', `/api/courses`);
-                if (result.error) {
-                    setError(new ApiError(result.error.message, result.error.status));
-                    return;
-                }
-
-                const fetchedCourses = result["hydra:member"].map((course: any) => convertToCourse(course));
-                setCourses(fetchedCourses);
-            } catch (err) {
-                setError(new ApiError((err as Error).message || "Unknown error", 500));
-            } finally {
-                setLoading(false);
+            const result = await request('GET', `/api/courses`);
+            if (!result) {
+                return null;
             }
+
+            const fetchedCourses = result["hydra:member"].map((course: any) => convertToCourse(course));
+            setCourses(fetchedCourses);
         };
 
         fetchData();
-    }, []);
+    }, [request]);
 
     if (loading) {
         return <Loading />;
-    }
-
-    if (error) {
-        return <ErrorPage detail={error.message} status={error.status} />;
     }
 
     return (
@@ -51,15 +39,21 @@ export default function CurriculumNavigator() {
             <div className="border rounded-md">
                 <CourseTableHeader />
 
-                {courses.length > 0 ? (
+                {error && (
+                    <div className="py-4 text-center">
+                        {t('unexpected')}
+                    </div>
+                )}
+
+                {!error && (courses.length > 0 ? (
                     courses.map((course) => (
                         <CourseRow key={course.id} course={course} />
                     ))
                 ) : (
                     <div className="py-4 text-center">
-                        No courses found matching your search.
+                        {t('curriculum-navigator.no-courses')}
                     </div>
-                )}
+                ))}
             </div>
         </div>
     );
