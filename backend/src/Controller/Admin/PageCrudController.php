f36dd0f2a0b64f54e49c8d8d1676062ce52f4c81
Robbe Serry <robbeserry@gmail.com>
2025-07-29 18:54:37 +0200

    BUR-330 Add filters to PageCrudController

--- backend/src/Controller/Admin/PageCrudController.php
+++ backend/src/Controller/Admin/PageCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Page;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
@@ -46,5 +47,16 @@ class PageCrudController extends AbstractCrudController
         yield TextEditorField::new('content_en')
             ->setLabel('Content (EN)')
             ->setTemplatePath('admin/text_editor.html.twig');
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('urlKey')
+            ->add('publicAvailable')
+            ->add('name_nl')
+            ->add('content_nl')
+            ->add('name_en')
+            ->add('content_en');
     }
 }
