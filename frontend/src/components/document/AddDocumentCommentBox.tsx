e9540c283b8a4581d4eebc1f329cd1718f17ed9d
Robbe Serry <robbeserry@gmail.com>
2025-08-01 12:51:37 +0200

    BUR-199 add translations and type for AddDocumentCommentBox

--- frontend/src/components/document/AddDocumentCommentBox.tsx
+++ frontend/src/components/document/AddDocumentCommentBox.tsx
@@ -1,26 +1,27 @@
 'use client'
 
-import React, {useState} from "react";
-import VoteButton from "@/components/ui/buttons/VoteButton";
 import AddDocumentCommentModal from "@/components/document/AddDocumentCommentModal";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-export default function AddDocumentCommentBox({ author, content, initialVotes }) {
+interface AddDocumentCommentBoxProps {
+    file?: string;
+    content?: string;
+}
+
+export default function AddDocumentCommentBox({ file, content }: AddDocumentCommentBoxProps) {
+    const { t } = useTranslation();
+
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const handleVote = async (delta: number) => {
-        return (vote: number) => {
-            console.log('vote registered:', delta, 'new vote:', vote);
-        };
-    };
 
     return (
         <div className="border rounded-lg max-w-sm p-2 min-h-20">
             <button
                 className="flex flex-row justify-between pb-2 text-gray-600"
                 onClick={() => setIsModalOpen(true)}>
-                Add comment...
+                <span>{t("document.comments.add")}</span>
             </button>
-            <AddDocumentCommentModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+            <AddDocumentCommentModal file={file} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
             <p className="text-sm">{content}</p>
         </div>
     );
