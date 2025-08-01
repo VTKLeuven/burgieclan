878f97139a281d46a233d382de3499f62a893a50
Robbe Serry <robbeserry@gmail.com>
2025-08-01 12:57:00 +0200

    BUR-199 add window title based on document name

--- frontend/src/app/[locale]/(header-footer)/document/[id]/page.tsx
+++ frontend/src/app/[locale]/(header-footer)/document/[id]/page.tsx
@@ -1,19 +1,16 @@
-'use client'
-
-import {useEffect, useState} from "react";
-import ErrorPage from "@/components/error/ErrorPage";
-import {ApiError} from "@/utils/error/apiError";
-import {ApiClient} from "@/actions/api";
-import DocumentCommentSection from "@/components/document/DocumentCommentSection";
 import DocumentPreview from "@/components/document/DocumentPreview";
-import LoadingPage from "@/components/loading/LoadingPage";
+import type { Metadata } from "next";
 
-export default function DocumentPage({ params }: { params: any }) {
+export const metadata: Metadata = {
+    title: "Document | Burgieclan",
+};
+
+export default function DocumentPage({ params }: { params: { id: string } }) {
     const { id } = params;
 
     return (
         <>
-            <DocumentPreview id={id}/>
+            <DocumentPreview id={id} />
         </>
     );
 }
