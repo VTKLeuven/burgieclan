acb8a39634573885693391e0116ea31297f20bb9
Robbe Serry <robbeserry@gmail.com>
2025-08-01 12:58:05 +0200

    BUR-199 pass file prop and add types

--- frontend/src/components/document/AddDocumentCommentModal.tsx
+++ frontend/src/components/document/AddDocumentCommentModal.tsx
@@ -1,14 +1,18 @@
-import Editor from "@/components/editor/Editor";
-import {Dialog} from "@/components/ui/Dialog";
 import PDFViewer from "@/components/document/pdf/PDFViewer";
-import {useEffect, useState, useRef} from "react";
+import Editor from "@/components/editor/Editor";
+import { Dialog } from "@/components/ui/Dialog";
+import { useEffect, useRef, useState } from "react";
 
-/**
- * TODO: improve layout, add submit button layout and functionality (sending comment to backend)
- * TODO: Rethink if this modal is really necessary or if we can just use the editor directly
- */
+//TODO: improve layout, add submit button layout and functionality (sending comment to backend)
+//TODO: Rethink if this modal is really necessary or if we can just use the editor directly
 
-export default function AddDocumentCommentModal({isModalOpen, setIsModalOpen}) {
+interface AddDocumentCommentModalProps {
+    file?: string;
+    isModalOpen: boolean;
+    setIsModalOpen: (isOpen: boolean) => void;
+}
+
+export default function AddDocumentCommentModal({ file, isModalOpen, setIsModalOpen }: AddDocumentCommentModalProps) {
     const [containerWidth, setContainerWidth] = useState<number>(0);
     const containerRef = useRef<HTMLDivElement>(null);
 
@@ -33,15 +37,17 @@ export default function AddDocumentCommentModal({isModalOpen, setIsModalOpen}) {
             isOpen={isModalOpen}
             onClose={() => setIsModalOpen(false)}
         >
-            <div
-                ref={containerRef}
-                className="flex-1 overflow-auto p-4"
-            >
-                <PDFViewer
-                    fileArg="/documents/test.pdf"
-                    width={containerWidth}
-                />
-            </div>
+            {file &&
+                <div
+                    ref={containerRef}
+                    className="flex-1 overflow-auto p-4"
+                >
+                    <PDFViewer
+                        file={file}
+                        width={containerWidth}
+                    />
+                </div>
+            }
             <div className="border-t border-gray-200 mt-2">
                 <Editor parentDialogOpen={isModalOpen} />
             </div>
--- frontend/src/components/document/DocumentComment.tsx
+++ frontend/src/components/document/DocumentComment.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import React from "react";
 import VoteButton from "@/components/ui/buttons/VoteButton";
+
+interface DocumentCommentProps {
+    author: string;
+    content: string;
+    initialVotes: number;
+}
 
 /**
  * Component for displaying a document comment
@@ -10,10 +15,10 @@ import VoteButton from "@/components/ui/buttons/VoteButton";
  * @param initialVotes  The initial number of votes for the comment
  * @constructor
  */
-export default function DocumentComment({ author, content, initialVotes }) {
+export default function DocumentComment({ author, content, initialVotes }: DocumentCommentProps) {
     const handleVote = async (delta: number) => {
         return (vote: number) => {
-            console.log('vote registered:', delta, 'new vote:', vote);
+            console.log('vote registered:', delta, 'new vote:', vote); // TODO replace with actual vote handling logic (best in the votebutton itself)
         };
     };
 
