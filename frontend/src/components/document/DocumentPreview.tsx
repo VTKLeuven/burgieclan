f5f58b9c2ee88d03b4e2cc8e0112bea0e39c2e33
Robbe Serry <robbeserry@gmail.com>
2025-08-01 12:56:12 +0200

    BUR-199 use useApi hook to get document and translations

--- frontend/src/components/document/DocumentPreview.tsx
+++ frontend/src/components/document/DocumentPreview.tsx
@@ -1,76 +1,49 @@
-import {Calendar, CircleUser, File, Package, ChartPie} from "lucide-react";
-import VoteButton, {VoteDirection} from "@/components/ui/buttons/VoteButton";
-import DocumentInfoField from "@/components/document/DocumentInfoField";
-import dynamic from "next/dynamic";
-import FavoriteButton from "@/components/ui/buttons/FavoriteButton";
-import DownloadButton from "@/components/ui/buttons/DownloadButton";
-import {useEffect, useState} from "react";
+'use client';
+
 import DocumentCommentSection from "@/components/document/DocumentCommentSection";
-import LoadingPage from "@/components/loading/LoadingPage";
-import ErrorPage from "@/components/error/ErrorPage";
-import {ApiClient} from "@/actions/api";
+import DocumentInfoField from "@/components/document/DocumentInfoField";
 import UnderReviewBox from "@/components/document/UnderReviewBox";
-import {useUser} from "@/components/UserContext";
+import ErrorPage from "@/components/error/ErrorPage";
+import LoadingPage from "@/components/loading/LoadingPage";
+import DownloadSingleDocumentButton from "@/components/ui/buttons/DownloadSingleDocumentButton";
+import VoteButton, { VoteDirection } from "@/components/ui/buttons/VoteButton";
+import FavoriteButton from "@/components/ui/FavoriteButton";
+import { useUser } from "@/components/UserContext";
+import { useApi } from "@/hooks/useApi";
+import type { Document } from "@/types/entities";
+import { convertToDocument } from "@/utils/convertToEntity";
+import { Calendar, ChartPie, CircleUser, File, Package } from "lucide-react";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
-/**
- * TODO:
- * -
- * -
- */
-
-interface DocumentData {
-    "@context": string;
-    "@id": string;
-    "@type": string;
-    name: string;
-    course: string;
-    category: string;
-    year: string;
-    under_review: boolean;
-    contentUrl: string;
-    mimetype: string;
-    filename : string;
-    creator: string;
-    createdAt: string;
-    updatedAt: string;
-}
-
-export default function DocumentPreview({id}: { id: string }) {
+export default function DocumentPreview({ id }: { id: string }) {
     // Lazy load PDFViewer component
-    const PDFViewer = dynamic(() => import("@/components/document/pdf/PDFViewer"), {ssr: false,});
+    const PDFViewer = dynamic(() => import("@/components/document/pdf/PDFViewer"), { ssr: false });
 
-    // Document data state
-    const [documentData, setDocumentData] = useState<DocumentData | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+    const [document, setDocument] = useState<Document | null>(null);
 
     // Used to scale pdf width to fit its parent container
     const [containerWidth, setContainerWidth] = useState<number>(0);
-    const [isAnonymous, setIsAnonymous] = useState(false);
 
-    // User data
     const { user } = useUser();
+    const { request, loading, error } = useApi();
+    const { t } = useTranslation();
 
     const MAXWIDTH = 1000;
 
     useEffect(() => {
         const fetchDocumentData = async () => {
-            try {
-                const result = await ApiClient('GET', `/api/documents/${id}`);
-                if (result.error) {
-                    setError('Failed to fetch document data');
-                    return;
-                }
-                setDocumentData(result);
-                setIsLoading(false);
-            } catch (err) {
-                setError('Failed to fetch document data');
-                setIsLoading(false);
+            const documentData = await request('GET', `/api/documents/${id}`);
+            if (!documentData) {
+                return null;
             }
+
+            setDocument(convertToDocument(documentData));
         };
 
         fetchDocumentData();
-    }, [id]);
+    }, [id, request]);
 
     // Update container width on mount and window resize, necessary to resize PDFViewer
     useEffect(() => {
@@ -84,46 +57,41 @@ export default function DocumentPreview({id}: { id: string }) {
         return () => window.removeEventListener('resize', updateWidth);
     }, []);
 
-    if (isLoading) return <LoadingPage className="w-full"/>
+    if (loading) return <LoadingPage />;
 
     if (error) {
         console.error(error);
-        return <ErrorPage className="w-full"/>;
+        return <ErrorPage status={error.status} detail={error.message} />;
     }
 
-    if (!documentData) return <div>No document data available</div>;
-
-    // Format the date for display
-    const formattedDate = new Date(documentData.createdAt).toLocaleDateString();
-
-    return (
+    return (document &&
         <div className="p-8 flex-auto text-sm w-full">
             {/* Filename */}
             <span className="inline-flex items-center space-x-4">
-                <File/>
-                <h3>{documentData.name}</h3>
+                <File />
+                <h3>{document.name}</h3>
             </span>
 
             {/* Description */}
             <div className="flex flex-row justify-between py-5">
                 <div className="flex space-x-8">
-                    <DocumentInfoField icon={Calendar} value={formattedDate}/>
-                    <DocumentInfoField icon={Package} value="4 MB"/>  {/*TODO: retrieve from api endpoint or calculate here*/}
-                    <DocumentInfoField icon={ChartPie} value={documentData.year}/>
+                    {document.createdAt && <DocumentInfoField icon={Calendar} value={document.createdAt?.toLocaleDateString()} />}
+                    <DocumentInfoField icon={Package} value="4 MB" />  {/*TODO: retrieve from api endpoint or calculate here*/}
+                    {document.year && <DocumentInfoField icon={ChartPie} value={document.year} />}
                 </div>
                 <div>
                     <DocumentInfoField
                         icon={CircleUser}
-                        value={isAnonymous ? "Anonymous" : documentData.creator}
+                        value={document.anonymous ? t("document.anonymous") : document.creator?.fullName || ""}
                     />
                 </div>
             </div>
 
             {/* Horizontal divider */}
-            <div className="w-full border-t border-vtk-blue-600 pb-5"/>
+            <div className="w-full border-t border-vtk-blue-600 pb-5" />
 
             {/* Under review box */}
-            {documentData.under_review && (
+            {document.underReview && (
                 <UnderReviewBox />
             )}
 
@@ -131,33 +99,31 @@ export default function DocumentPreview({id}: { id: string }) {
             <div className="flex flex-row space-x-4 w-full justify-center">
                 <div style={{ width: containerWidth }} className="py-5">
                     <div className="flex flex-row h-8 justify-between place-items-center">
-                        <VoteButton
+                        <VoteButton /* TODO: implement voting functionality */
                             initialVotes={10}
                             initialVote={VoteDirection.UP}
                             disabled={!user}
                             className="border-gray-500"
                         />
                         <div className="flex space-x-2">
-                            <DownloadButton
-                                contentUrl={documentData.contentUrl}
-                                fileName={documentData.filename}
-                                fileSize="3.6 MB"
+                            <DownloadSingleDocumentButton
+                                document={document}
+                                fileSize="3.6 MB" /* TODO: retrieve from api endpoint or calculate here*/
                                 disabled={!user}
                             />
                             <FavoriteButton
-                                favoriteType="documents"
-                                resourceId={Number(id)}
-                                disabled={!user}
+                                itemId={Number(id)}
+                                itemType="document"
                             />
                         </div>
                     </div>
 
                     {/*TODO: expand preview to other file types*/}
-                    {(documentData && documentData.mimetype == "application/pdf") ? (
-                        <PDFViewer fileArg={process.env.NEXT_PUBLIC_BACKEND_URL + documentData.contentUrl} width={containerWidth} />
+                    {(document && document.mimetype == "application/pdf" && document.contentUrl) ? (
+                        <PDFViewer file={document.contentUrl} width={containerWidth} />
                     ) : (
                         <div className="w-full h-96 flex items-center justify-center">
-                            <p>No preview available</p>
+                            <p>{t('document.no-preview')}</p>
                         </div>
                     )}
                 </div>
