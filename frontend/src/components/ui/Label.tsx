8062227c79fa9bc7d35f9c379ae0c6d6ff63c960
Robbe Serry <robbeserry@gmail.com>
2025-09-04 16:10:38 +0200

    BUR-222 reorganize imports for consistency and clarity

--- frontend/src/components/ui/Label.tsx
+++ frontend/src/components/ui/Label.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import * as React from "react"
 import { cn } from "@/lib/utils"
+import * as React from "react"
 
 export interface LabelProps
-  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+  extends React.LabelHTMLAttributes<HTMLLabelElement> { }
 
 const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
   ({ className, ...props }, ref) => {
