a7a4dff5562d441f03ca8b5c8a6ecaf0919aec3c
Robbe Serry <robbeserry@gmail.com>
2025-09-20 12:08:46 +0200

    BUR-177: all lang parameters to announcement api

--- backend/src/Mapper/AnnouncementEntityToApiMapper.php
+++ backend/src/Mapper/AnnouncementEntityToApiMapper.php
@@ -33,10 +33,10 @@ class AnnouncementEntityToApiMapper implements MapperInterface
         assert($from instanceof Announcement);
         assert($to instanceof AnnouncementApi);
 
-        $to->title_nl = $from->getTitleNl();
-        $to->content_nl = $from->getContentNl();
-        $to->title_en = $from->getTitleEn();
-        $to->content_en = $from->getContentEn();
+        $lang = $context['lang'] ?? Announcement::$DEFAULT_LANGUAGE;
+
+        $to->title = $from->getTitle($lang);
+        $to->content = $from->getContent($lang);
         $to->priority = $from->isPriority();
 
         $to->creator = $this->microMapper->map($from->getCreator(), UserApi::class, [
@@ -45,8 +45,8 @@ class AnnouncementEntityToApiMapper implements MapperInterface
 
         $to->startTime = $from->getStartTime()->format('Y-m-d H:i:s');
         $to->endTime = $from->getEndTime()->format('Y-m-d H:i:s');
-        $to->createdAt = DateTime::createFromInterface($from->getCreateDate());
-        $to->updatedAt = DateTime::createFromInterface($from->getUpdateDate());
+        $to->createdAt = DateTime::createFromInterface($from->getCreateDate())->format('Y-m-d H:i:s');
+        $to->updatedAt = DateTime::createFromInterface($from->getUpdateDate())->format('Y-m-d H:i:s');
 
         return $to;
     }
