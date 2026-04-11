2ba02e06b476ef3a72f894575ba5b95e8a8c4d6f
Robbe Serry <robbeserry@gmail.com>
2026-04-11 14:14:30 +0200

    BUR-418 Update dependencies in composer.lock to latest versions

--- backend/src/Command/ListUsersCommand.php
+++ backend/src/Command/ListUsersCommand.php
@@ -20,8 +20,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 /**
  * A console command that lists all the existing users.
