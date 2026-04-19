40cf033c82fd2cb4d6db4fd14aacaf1280e8def9
Robbe Serry <robbeserry@gmail.com>
2026-04-19 16:00:14 +0200

    BUR-422 Remove bootstrap-tagsinput integration and related styles from the backend

--- backend/assets/admin.js
+++ backend/assets/admin.js
@@ -1,37 +1,5 @@
 import { Modal } from 'bootstrap';
-import 'bootstrap-tagsinput';
 import './styles/admin.scss';
-
-function buildLocalTagMatcher(tags) {
-    return function findMatches(query, syncResults) {
-        if (!query) {
-            syncResults(tags);
-            return;
-        }
-
-        const normalizedQuery = query.toLowerCase();
-        const matches = tags.filter((tag) => tag.toLowerCase().includes(normalizedQuery));
-        syncResults(matches);
-    };
-}
-
-$(function () {
-    // Bootstrap-tagsinput initialization
-    // https://bootstrap-tagsinput.github.io/bootstrap-tagsinput/examples/
-    var $input = $('input[data-toggle="tagsinput"]');
-    if ($input.length) {
-        var tags = $input.data('tags') || [];
-
-        $input.tagsinput({
-            trimValue: true,
-            focusClass: 'focus',
-            typeaheadjs: {
-                name: 'tags',
-                source: buildLocalTagMatcher(tags)
-            }
-        });
-    }
-});
 
 // Handling the modal confirmation message.
 $(document).on('submit', 'form[data-confirmation]', function (event) {
