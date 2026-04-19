11c945d8ea3ec39cede532c9929d4fc8d9eab294
Robbe Serry <robbeserry@gmail.com>
2026-04-19 16:00:14 +0200

    BUR-422 Remove bootstrap-tagsinput integration and related styles from the backend

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -68,9 +68,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
-    .autoProvideVariables({
-        "jQuery.tagsinput": "bootstrap-tagsinput"
-    })
     ;
 
 module.exports = Encore.getWebpackConfig();
